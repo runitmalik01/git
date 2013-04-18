f03cf80cc2ddffebee2eb8c3e632b684665b6b78
Pankaj <psingh@mootly.com>
2013-04-18 10:19:02 +0000

    added new SelfAssesmentTax.java

---
+++ site/src/main/java/com/mootly/wcm/member/SelfAssesmentTax.java
@@ -0,0 +1,50 @@
+package com.mootly.wcm.member;
+
+
+import org.hippoecm.hst.core.component.HstComponentException;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mootly.wcm.annotations.ChildBean;
+import com.mootly.wcm.annotations.DataTypeValidationFields;
+import com.mootly.wcm.annotations.DataTypeValidationHelper;
+import com.mootly.wcm.annotations.FormFields;
+import com.mootly.wcm.annotations.PrimaryBean;
+import com.mootly.wcm.annotations.RequiredFields;
+import com.mootly.wcm.beans.SelfAssesmetTaxDocument;
+import com.mootly.wcm.beans.compound.SelfAssesmentTaxDetail;
+import com.mootly.wcm.components.ITReturnComponent;
+
+/*
+ * Author:Pankaj Singh
+ * Date:13/3/2013
+ * Description:It will taje value from Tdsfromsalary.jsp and pass it to bean
+ */
+@PrimaryBean(primaryBeanClass=SelfAssesmetTaxDocument.class)
+@ChildBean(childBeanClass=SelfAssesmentTaxDetail.class)
+
+@FormFields(fieldNames={"bsr_code","date_credit","Serial_challan","amount"})
+@RequiredFields(fieldNames={"bsr_code","date_credit","Serial_challan","amount"})
+@DataTypeValidationFields(fieldNames={"bsr_code"},dataTypes={DataTypeValidationHelper.DataTypeValidationType.BSR})
+public class SelfAssesmentTax extends ITReturnComponent {
+	
+	private static final Logger log = LoggerFactory.getLogger(TdsFromSalary.class);
+	public void doBeforeRender(HstRequest request, HstResponse response) {
+		// TODO Auto-generated method stub
+		super.doBeforeRender(request, response);
+		System.out.println("this is do before render of self assesment tax");
+	}
+	@Override
+	public void doAction(HstRequest request, HstResponse response)
+			throws HstComponentException {
+		
+		// TODO Auto-generated method stub
+		super.doAction(request, response);
+		System.out.println("this is do Action of self assesment tax");
+	} }
+	
+	
+	
+
