a3f241ba7596b431aadf7d4f733ace51df02b176
Dhananjay <dpanwar@mootly.com>
2013-04-18 10:58:37 +0000

    changes made for interest document

--- site/src/main/java/com/mootly/wcm/beans/InterestDocument.java
+++ site/src/main/java/com/mootly/wcm/beans/InterestDocument.java
@@ -5,18 +5,23 @@
  */
 
 package com.mootly.wcm.beans;
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Value;
+
+import org.hippoecm.hst.component.support.forms.FormMap;
 import org.hippoecm.hst.content.beans.ContentNodeBinder;
 import org.hippoecm.hst.content.beans.ContentNodeBindingException;
 import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 
 @SuppressWarnings("unused")
 @Node(jcrType = "mootlywcm:interestdocument")
-public class InterestDocument extends BaseDocument implements ContentNodeBinder {
+public class InterestDocument extends BaseDocument implements ContentNodeBinder,FormMapFiller {
 	static final public String NAMESPACE = "mootlywcm:interestdocument";
 	static final public String NODE_NAME = "interestdocument";
 	private String section234A;
@@ -51,22 +56,47 @@ public class InterestDocument extends BaseDocument implements ContentNodeBinder 
 		this.section234C = Section234C;
 	}
    
+	@SuppressWarnings("deprecation")
 	@Override
 	public boolean bind(Object content, javax.jcr.Node node)
 			throws ContentNodeBindingException {
 		// TODO Auto-generated method stub
 		try {
 			InterestDocument interestdocument = (InterestDocument) content;
+			
 			node.setProperty("mootlywcm:section234A",interestdocument.getSection234A());
 	    	node.setProperty("mootlywcm:section234B",interestdocument.getSection234B());
 	    	node.setProperty("mootlywcm:section234C",interestdocument.getSection234C());
 	    	
-	    	 	
-	    
     	}catch (RepositoryException re) {
     		log.error("Binding Node Error",re);
     		
     	}
 		return true;
+	}
+	
+	@Override
+	public void fill(FormMap formMap) {
+		// TODO Auto-generated method stub
+		if (log.isInfoEnabled()) {
+			log.info("Into the fill method");			
+		}
+		if (formMap == null) return;
+		if ( formMap.getField("section234A") != null) {
+			setSection234A(formMap.getField("section234A").getValue());
+		}
+		if ( formMap.getField("section234B") != null) {
+			setSection234B(formMap.getField("section234B").getValue());
+		}
+		if ( formMap.getField("section234C") != null) {
+			setSection234C(formMap.getField("section234C").getValue());
+		}
+		
+	}
+	
+	@Override
+	public <T extends HippoBean> void cloneBean(T sourceBean) {
+		// TODO Auto-generated method stub
+		
 	}
 }
