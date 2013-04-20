2daeb738674938ac8db5abd50b8617c56b1b544c
Kusum <klata@mootly.com>
2013-04-20 10:56:45 +0000

    Calculators

--- site/src/main/webapp/js/taxcalculator.js
+++ site/src/main/webapp/js/taxcalculator.js
@@ -1,9 +1,63 @@
 /*
- * Java script for Tax Calculator
+ * Java script for all calculators 
  * 
+ * On Fly calculations Functions
  * 
  * */
+ 
+  
+ // EMI Calculator
+function fill() {
+    var A= document.getElementById("A").value-0;
+    var B = document.getElementById("B").value-0;
+		var C= document.getElementById("C").value-0;
+		var  a = document.getElementById("D").value-0;
+		a = a || 0;
+		if(A>0 && B>0 && C>0){
+		var id1= A*(B/1200);
+		var id2= (B/1200)+1;
+		var id3= Math.pow((id2), C);
+		var id4= id3-1;
+		var id5= id3/id4;
+		var id6= Math.round((id1*id5)* 100) / 100;
+    document.getElementById("D").value = (id6) ;
+		}
+}; // End Of function fill.id3=(1+(B/1200)
 
+
+
+
+// NPV Calculator
+	function call() {
+		var a = document.getElementById("initial_invest").value-0;
+		var rate = document.getElementById("rate").value-0;
+		var a1 = document.getElementsByName("cashflow1").value-0;
+		var s1 = 0;
+		if(a>0 && rate>0){
+			for (var caflow = 0; caflow <a1.length; caflow++) {
+				s1 = s1 + ( a1.item(caflow).value / (Math.pow((1 + (rate/100)),caflow+1)));
+			}
+			s1=parseFloat(s1)-parseFloat(a);;
+			document.getElementById("calculate").value = Math.round((s1)* 100) / 100;
+		}//End of if (for checking input values should be greater then 0)	
+	};// End Of function call
+	
+	function no_of_year() {
+		var year = $("#no_of_year").val();
+		if (year != null) {
+			window.location.replace("http://localhost:8080/site/npvcalculator?year="+year);	
+		}
+	};// End Of function no_of_year.
+
+	function back(){
+		window.location.replace("http://localhost:8080/site/npvcalculator");	
+		};// End Of function back.
+		
+		
+
+
+  
+ //Tax Calculator
 function calculateTax() {
 	var assyear = document.getElementById("cbassyear").value;
 	var payer = document.getElementById("cbasstype").value;
@@ -907,3 +961,21 @@ function display() {
 
 	}
 }; // End of display function (to hide gender and residential status.
+
+
+
+//For number inputs with decimal only.....
+function isNumberKey(evt)
+{
+   var charCode = (evt.which) ? evt.which : event.keyCode ;
+   if (charCode != 46 && charCode > 31  && (charCode < 48 || charCode > 57)){
+	   return false;   
+   } 
+   return true;
+  
+};
+
+
+
+
+
