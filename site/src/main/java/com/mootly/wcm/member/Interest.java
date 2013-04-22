f1bb8ce819314d34a6f502101433f57197873872
Dhananjay <dpanwar@mootly.com>
2013-04-22 11:30:26 +0000

    changes made for interest

--- site/src/main/java/com/mootly/wcm/member/Interest.java
+++ site/src/main/java/com/mootly/wcm/member/Interest.java
@@ -13,7 +13,7 @@ import com.mootly.wcm.annotations.AdditionalBeans;
 import com.mootly.wcm.annotations.FormFields;
 import com.mootly.wcm.annotations.PrimaryBean;
 import com.mootly.wcm.annotations.RequiredBeans;
-import com.mootly.wcm.beans.InterestDocument;
+import com.mootly.wcm.beans.InterestDoc;
 import com.mootly.wcm.beans.MemberPersonalInformation;
 import com.mootly.wcm.components.ITReturnComponent;
 import org.hippoecm.hst.core.component.HstComponentException;
@@ -22,10 +22,10 @@ import org.hippoecm.hst.core.component.HstResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@PrimaryBean(primaryBeanClass=InterestDocument.class)
+@PrimaryBean(primaryBeanClass=InterestDoc.class)
 @AdditionalBeans(additionalBeansToLoad=MemberPersonalInformation.class)
 @RequiredBeans(requiredBeans={MemberPersonalInformation.class})
-@FormFields(fieldNames={"section234A","section234B","section234C"})
+@FormFields(fieldNames={"section234A","section234B","section234C","section234ABC"})
 
 public class Interest extends ITReturnComponent {
 
