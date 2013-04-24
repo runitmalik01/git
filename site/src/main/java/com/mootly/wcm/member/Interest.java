2bd382cec15e032d9f58fb102513839876a3f131
Dhananjay <dpanwar@mootly.com>
2013-04-24 04:44:26 +0000

    changes made for double

--- site/src/main/java/com/mootly/wcm/member/Interest.java
+++ site/src/main/java/com/mootly/wcm/member/Interest.java
@@ -36,93 +36,10 @@ public class Interest extends ITReturnComponent {
 
 	private static final Logger log = LoggerFactory.getLogger(Interest.class);
 
-	Member member = null;
-	String filing_year = null;
-	String modusername= null;
-	String pan = null;
-	Session persistableSession = null;
-
 	@Override
 	public void doBeforeRender(HstRequest request, HstResponse response) {
 		super.doBeforeRender(request, response);
-		//fetching values for calculation
-
-		member=(Member)request.getSession().getAttribute("user");
-		/*pan=(String) request.getSession().getAttribute("pan");
-		if(null == pan)
-		{
-			pan ="abcdb1234a";
-		}
-		filing_year=(String) request.getSession().getAttribute("filing_year");
-		if(null == filing_year)
-		{
-			filing_year ="2012-2013";
-		}*/
-
-		String username=member.getUserName().trim();
-		modusername=username.replaceAll("@", "-at-").trim();
-		filing_year = request.getRequestContext().getResolvedSiteMapItem().getParameter("assessmentYear");
-		//itReturnType = request.getRequestContext().getResolvedSiteMapItem().getParameter("itReturnType"); //original versus amend
-		pan = request.getRequestContext().getResolvedSiteMapItem().getParameter("pan"); //original versus amend
-		log.info("inside fetchSalaryIncomeDocument--->member:-"+member);
-		log.info("inside fetchSalaryIncomeDocument--->member:-"+filing_year);
-		log.info("inside fetchSalaryIncomeDocument--->member:-"+pan);
-		if(member!=null){
-
-			Calculations cal=new Calculations();
-			// fetching Salary Income value
-			double fSalaryIncome = cal.fetchSalaryIncomeValue(request,response);
-			// fetching OtherIncome value
-			double fOtherIncome = cal.fetchOtherIncomeValue(request,response);
-			//fetching house property values
-			double fHouseProperty = cal.fetchHousePropertyValue(request,response);
-			//fetching capital gain values
-			double fCapitalGain = cal.fetchCapitalGainValue(request,response);
-			//fetching TCS document values
-			double fTcsDoc = cal.fetchTcsDocumentValue(request,response);
-			// fetching Deduction  value
-			double fDeduction = cal.fetchDeductionsValue(request,response);
-			// fetching Losses value
-			double fAdjustLosses =cal.fetchLossesValue(request,response);
-			// fetching Securities value
-			double fSecurities = cal.fetchSecurityValue(request,response);
-			// fetching Interest value
-			double fInterest = cal.fetchinterestValue(request,response);
-			// fetching Rebate89  value
-			double fRebate89 = cal.fetchrebate89Value(request,response);
-			// fetching Rebate 90-91 value
-			double fRebate90_91 = cal.fetchrebate90_91Value(request,response);
-			// fetching TDS from salary value
-			double ftdsSalary=cal.fetchTdsfromsalaryValue(request,response);
-
-			double flessRebate=fRebate89 + fRebate90_91;
-			double ftdsother=cal.fetchTdsOtherValue(request,response);
-			double fTotal= fSalaryIncome + fOtherIncome + fHouseProperty + fCapitalGain + fSecurities;
-			log.info("Total of all"+fTotal);
-			log.info("big decimal"+BigDecimal.valueOf(fTotal).toPlainString());
-			double fGrossTotal = fTotal-fAdjustLosses;
-			double fTaxableIncome= fGrossTotal-fDeduction;
-			//double fIncomeTax=(double)Math.round(fTaxableIncome*(0.1f));
-			// for fetching income tax according to slab rates
-			double fIncomeTax= cal.fetchIncomeTaxValue(request,response,fTaxableIncome);
-			log.info("income tax is"+fIncomeTax);
-			double fEduCess=(double) Math.round(fIncomeTax*0.03f);
-			double fIncomeTaxEduCess=fIncomeTax + fEduCess ;
-			double fTaxafterrebate= fIncomeTaxEduCess - flessRebate;
-			double fselfasses =0.0f;
-			double fLessPrepaidTax = ftdsother +  ftdsSalary + fTcsDoc+ fselfasses;
-			double fTax_Payable =(fTaxafterrebate + fInterest - fLessPrepaidTax);
-			double fNormaltax= 0.0f;
-			double fSpecialtax= 0.0f;
-			double fSurcharge= 0.0f;
-			log.info("dhvdhdg################"+fCapitalGain);
-			int decimalPlace = 2;
-
-			double fTaxLiability=fTaxafterrebate-fLessPrepaidTax;;
-			log.info("tax liabilityyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"+fTaxLiability);
-			request.setAttribute("taxliability", fTaxLiability);
-
-		}
+	
 	}
 
 	@Override
