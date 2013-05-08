254b604fdcf3bc9fde93045926a7b51a83cc3d68
Dhananjay <dpanwar@mootly.com>
2013-05-08 12:00:58 +0000

    changes made for xml

--- site/src/main/java/com/mootly/wcm/beans/AdvanceTaxDocument.java
+++ site/src/main/java/com/mootly/wcm/beans/AdvanceTaxDocument.java
@@ -27,6 +27,8 @@
 package com.mootly.wcm.beans;
 import static com.mootly.wcm.utils.Constants.NT_PERSONAL_INFO_LINK;
 
+import java.math.BigInteger;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -90,6 +92,18 @@ public class AdvanceTaxDocument extends BaseDocument implements ContentNodeBinde
 	    	if (total_amount == null) total_amount = getProperty("mootlywcm:totalamount");
 	    	return total_amount;
 	 }
+        // created for xml
+		public BigInteger getBigTotal_Amount() {
+			if (total_amount == null) total_amount = getProperty("mootlywcm:totalamount");
+			DecimalFormat decimalFormat=new DecimalFormat("#.#");
+			BigInteger bigTotal=null;
+			if(total_amount!=null && total_amount.toString().length()>0){
+				bigTotal = new BigInteger(decimalFormat.format(total_amount));
+				return bigTotal;
+			}else
+				return null;
+		}
+		
 	   public final void setTotal_Amount(double sum) {
 			this.total_amount = sum;
 		}
