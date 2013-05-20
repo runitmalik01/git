f86d8504e2fb1d2b0e8bf79587c38258c260deaf
Priyank Gupta <pgupta@mootly.com>
2013-05-20 07:48:25 +0000

    added screen calculation logic

--- site/src/main/java/com/mootly/wcm/member/SalaryIncome.java
+++ site/src/main/java/com/mootly/wcm/member/SalaryIncome.java
@@ -9,16 +9,19 @@
 
 package com.mootly.wcm.member;
 
+import java.util.Map;
+
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.core.component.HstComponentException;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mootly.wcm.annotations.AdditionalBeans;
 import com.mootly.wcm.annotations.ChildBean;
 import com.mootly.wcm.annotations.DataTypeValidationFields;
-import com.mootly.wcm.annotations.DataTypeValidationHelper;
 import com.mootly.wcm.annotations.DataTypeValidationType;
 import com.mootly.wcm.annotations.FormFields;
 import com.mootly.wcm.annotations.PrimaryBean;
@@ -26,20 +29,70 @@ import com.mootly.wcm.annotations.RequiredBeans;
 import com.mootly.wcm.annotations.RequiredFields;
 import com.mootly.wcm.beans.MemberPersonalInformation;
 import com.mootly.wcm.beans.SalaryIncomeDocument;
+import com.mootly.wcm.beans.ScreenCalculation;
 import com.mootly.wcm.beans.compound.SalaryIncomeDetail;
 import com.mootly.wcm.components.ITReturnComponent;
+import com.mootly.wcm.services.ScreenCalculatorService;
 @PrimaryBean(primaryBeanClass=SalaryIncomeDocument.class)
 @ChildBean(childBeanClass=SalaryIncomeDetail.class)
+@AdditionalBeans(additionalBeansToLoad=MemberPersonalInformation.class)
+@RequiredBeans(requiredBeans={MemberPersonalInformation.class})
+@RequiredFields(fieldNames={"Name_employer","Tan_employer","City","Gross_salary"})
+@DataTypeValidationFields(fieldNames={
+		"Gross_salary",
+		"Allowance",
+		"Allowance1",
+		"Perquisite",
+		"Profit",
+		"Taxable_earning",
+		"Tan_employer",
+		"Pan_employer",
+		"Pan_employee",
+		"From",
+		"To"
+},
+dataTypes= {
+		DataTypeValidationType.DECIMAL, //Gross_salary
+		DataTypeValidationType.DECIMAL,
+		DataTypeValidationType.DECIMAL,
+		DataTypeValidationType.DECIMAL,
+		DataTypeValidationType.DECIMAL,
+		DataTypeValidationType.DECIMAL,
+		DataTypeValidationType.TAN,
+		DataTypeValidationType.PAN,
+		DataTypeValidationType.PAN,
+		DataTypeValidationType.INDIANDATE,
+		DataTypeValidationType.INDIANDATE
+}
+		)
 
 @FormFields(fieldNames={"Employe_category","Name_employer","Name_employee","Pan_employer","Tan_employer","Pan_employee","Addressslry",
 		"Cityslry","Stateslry","Pinslry","From","To","Gross_salary","Allowance","Perquisite","profit","Taxable_earning"})
 
 public class SalaryIncome extends ITReturnComponent {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(SalaryIncome.class);
+
+	@Override
 	public void doBeforeRender(HstRequest request, HstResponse response) {
-		super.doBeforeRender(request, response);
-		System.out.println("this is do before render of salary income");
+		// TODO Auto-generated method stub
+		super.doBeforeRender(request, response);	
+		String isCalc = getPublicRequestParameter(request,"command");
+		if (isCalc != null && isCalc.equals("calc")) {
+			String pathToScreenCalc = "configuration/screencalculation/" + this.getClass().getSimpleName().toLowerCase();
+			HippoBean siteContentBaseBean=(HippoBean)request.getAttribute("siteContentBaseBean");
+			ScreenCalculation screencalc=siteContentBaseBean.getBean(pathToScreenCalc, ScreenCalculation.class);
+			Map<String,Object> resultSet = ScreenCalculatorService.getScreenCalculations(screencalc.getScript(), request.getParameterMap(""), null);
+			if (resultSet != null) {
+				log.info("get the result");
+				request.setAttribute("resultSet", resultSet);
+				JSONObject jsonObject  = new JSONObject(resultSet);
+				log.info("get the json object"+jsonObject.toString());
+				request.setAttribute("jsonObject", jsonObject);
+				//response.setContentType("application/json");
+				response.setRenderPath("jsp/common/calculation_response.jsp");
+			}
+		}
 	}
 
 	@Override
