6e804e7ba30107278e28ba4805f4f35d6fa6d55d
Dhananjay <dpanwar@mootly.com>
2013-05-21 07:59:27 +0000

    changes made to remove decimal

--- site/src/main/java/com/mootly/wcm/services/IndianCurrencyHelper.java
+++ site/src/main/java/com/mootly/wcm/services/IndianCurrencyHelper.java
@@ -1,12 +1,41 @@
 package com.mootly.wcm.services;
 
+import static com.mootly.wcm.utils.Constants.SERIAL;
+
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public final class IndianCurrencyHelper {
-	public static Double round(Double in) {
-		BigDecimal bd = new BigDecimal(in).setScale(2, RoundingMode.HALF_EVEN);
-		Double d = bd.doubleValue();
-		return d;
+
+	//method to convert double value into Biginteger
+	public BigInteger bigIntegerRound(Double in) {
+		BigDecimal bd = new BigDecimal(in).setScale(0, RoundingMode.HALF_EVEN);
+		DecimalFormat decimalFormat=new DecimalFormat("#.#");
+		BigInteger bigTotal=null;
+		if(bd!=null && bd.toString().length()>0){
+			bigTotal = new BigInteger(decimalFormat.format(bd));		
+			return bigTotal;
+		}else
+			return null;
 	}
+
+	// method to convert double value into Long
+	public Long longRound(Double in){
+		BigDecimal bd = new BigDecimal(in).setScale(0, RoundingMode.HALF_EVEN);
+		return bd.longValue();
+	}
+
+	// method to convert String value into Biginteger
+	public BigInteger bigIntegerRoundStr(String in){
+
+		BigInteger bigStr=null;
+		if(in!=null && in.length()>0){
+			bigStr = new BigInteger(in);
+			return bigStr;
+		}else
+			return null;
+	}
+
 }
