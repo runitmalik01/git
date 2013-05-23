c8c60bca93a603bd27702fbca8c0cbb905e9d645
Priyank Gupta <pgupta@mootly.com>
2013-05-23 21:21:28 +0000

    change in salaryincome calculation

--- site/src/main/resources/scripts/salaryincome.js
+++ site/src/main/resources/scripts/salaryincome.js
@@ -4,10 +4,4 @@
  * 
  * **/
 
-/*var A = document.getElementById("Gross_salary").value - 0;
-var B = document.getElementById("Allowance").value - 0;
-var C = document.getElementById("Perquisite").value - 0;
-var D = document.getElementById("profit").value - 0;
-document.getElementById("Taxable_earning").value = (A + B + C + D);*/
-
 out_Taxable_earning=(Gross_salary-0)+(Allowance-0)+(Perquisite-0)+(profit-0);
