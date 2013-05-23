d236c808e075953a6f40541220fc08f9e95c4faa
Priyank Gupta <pgupta@mootly.com>
2013-05-23 06:05:43 +0000

    screen calculation for form16

---
+++ site/src/main/resources/scripts/form16.js
@@ -0,0 +1,28 @@
+/**
+ * This file is used for calculation on Form 16 Screen   
+ * 
+ * 
+ */
+
+
+out_gross_total=gross_a+gross_b+gross+c;
+
+out_balance=gross_total-(less_total_2+less_total_1);
+
+out_deductions_total=deductions_tax+deductions_entertainment;
+
+out_income_chargable_total=balance-deductions_total;
+
+out_gross_income_total=income_chargable_total+additional_2+additional_1;
+
+out_total_income_2=gross_income_total-aggregate;
+
+
+
+out_tax_total_income_2=
+
+out_tax_payable=tax_total_income_2+surcharge_2+education_cess;
+
+out_tax_payable_2=tax_payable-relief_2;
+
+out_relief_12=tax_payable_2-(ded_ent2+ded_ent4);
