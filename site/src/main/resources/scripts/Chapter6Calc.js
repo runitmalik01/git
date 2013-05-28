67501d03b0127f3336968925f3c725b89e673112
Priyank Gupta <pgupta@mootly.com>
2013-05-28 10:55:39 +0000

    added all section calculation

--- site/src/main/resources/scripts/Chapter6Calc.js
+++ site/src/main/resources/scripts/Chapter6Calc.js
@@ -16,7 +16,7 @@ out_total_eligiblededuction=0;
 //isSeniorCitizen=true;
 /** - END Configuration */
 //80E-It depends on gross total income
-var grosstotal=(salarypension-0) + (othersources-0) + (housproperty-0);
+var grosstotal= salarypension + othersources + houseproperty;
 
 //--- This starts 80c,80ccc,80D Employee's Contribution
 var doubleArray = java.lang.reflect.Array.newInstance(java.lang.Double, 3);
@@ -50,6 +50,7 @@ if(total_80ccd_1 >= tenperSalincome)
 	out_total_80ccd_1=tenperSalincome;
 else out_total_80ccd_1=total_80ccd_1;
 //--this one is 80CCF this upper limit it 20000
+out_total_80ccf=0;
 if (total_80ccf > maxAllowed_80CCF) out_total_80ccf = maxAllowed_80CCF;
 
 //medical insurance premium
@@ -116,19 +117,22 @@ out_total_80ggc= (total_80ggc < grosstotal) ? total_80ggc:grosstotal;
 out_total_80jja= (total_80jja < grosstotal) ? total_80jja:grosstotal;
 
 //out_total_80ia= (total_80ia < grosstotal) ? total_80ia:grosstotal;
-	
+
 out_total_eligiblededuction = out_total_80c + out_total_80ccc + out_total_80ccd_1 + out_total_80ccd_2 + out_total_80qqb + out_total_80rrb + out_total_80gga + out_total_80ggc + out_total_80g + out_total_80jja + out_total_80ddb + out_total_80u + out_total_80dd + out_total_80qqb + out_total_80e ;
 
 //80G- Calculate AdjustedGrossTotal and Excess Rent Paid and 2000 per Month
-var adjuestedGrossTotal=grosstotal - out_total_eligiblededuction;
-var adjuestedGross10per=adjuestedGrossTotal * 0.1;
-var	adjuestedGross25per=adjuestedGrossTotal * 0.25;
-var excessRentPaid=total_80gg-adjuestedGross10per;
-var rent2000permnth=24000;
-out_total_80gg=Math.min(rent2000permnth,adjuestedGross25per,excessRentPaid);
-if(out_total_80gg<0)
-	out_total_80gg=0;
-out_total_eligiblededuction=out_total_eligiblededuction+out_total_80gg;
+out_total_80gg=0;
+if(total_80gg>0){
+	var adjuestedGrossTotal=grosstotal - out_total_eligiblededuction;
+	var adjuestedGross10per=adjuestedGrossTotal * 0.1;
+	var	adjuestedGross25per=adjuestedGrossTotal * 0.25;
+	var excessRentPaid=total_80gg-adjuestedGross10per;
+	var rent2000permnth=24000;
+	out_total_80gg=Math.min(rent2000permnth,adjuestedGross25per,excessRentPaid);
+	if(out_total_80gg<0)
+		out_total_80gg=0;
+	out_total_eligiblededuction=out_total_eligiblededuction+out_total_80gg;
+}
 if(out_total_eligiblededuction>grosstotal)
 	out_total_eligiblededuction=grosstotal;
 //print("hello word"+grosstotal);
