27498c78121b91ad0cfee981fb8497324a57e52d
Dhananjay <dpanwar@mootly.com>
2013-05-28 07:42:38 +0000

    added a new common method for calendar

--- site/src/main/java/com/mootly/wcm/services/IndianCurrencyHelper.java
+++ site/src/main/java/com/mootly/wcm/services/IndianCurrencyHelper.java
@@ -1,25 +1,39 @@
 package com.mootly.wcm.services;
 
+import static com.mootly.wcm.utils.Constants.DATE;
 import static com.mootly.wcm.utils.Constants.SERIAL;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 public final class IndianCurrencyHelper {
 
-	//method to convert double value into Biginteger
+
+	/**
+	 * This Method is used to Round the Decimal Values and convert it into BigIngteger
+	 * @return BigInteger
+	 * @param double
+	 * 
+	 * */
 	public BigInteger bigIntegerRound(Double in) {
 		if(in!=null){
-		BigDecimal bd = new BigDecimal(in).setScale(0, RoundingMode.HALF_EVEN);
-		DecimalFormat decimalFormat=new DecimalFormat("#.#");
-		BigInteger bigTotal=null;
-		if(bd!=null && bd.toString().length()>0){
-			bigTotal = new BigInteger(decimalFormat.format(bd));		
-			return bigTotal;
-		}else
-			return null;
+			BigDecimal bd = new BigDecimal(in).setScale(0, RoundingMode.HALF_EVEN);
+			DecimalFormat decimalFormat=new DecimalFormat("#.#");
+			BigInteger bigTotal=null;
+			if(bd!=null && bd.toString().length()>0){
+				bigTotal = new BigInteger(decimalFormat.format(bd));		
+				return bigTotal;
+			}else
+				return null;
 		}else
 			return null;
 	}
@@ -38,15 +52,23 @@ public final class IndianCurrencyHelper {
 		return Double.valueOf(df2.format(val));
 	}
 
-
-
-	// method to convert double value into Long
+	/**
+	 * This Method is used to Round the Decimal Values and convert it into Long
+	 * @return Long
+	 * @param double
+	 * 
+	 * */
 	public Long longRound(Double in){
 		BigDecimal bd = new BigDecimal(in).setScale(0, RoundingMode.HALF_EVEN);
 		return bd.longValue();
 	}
 
-	// method to convert String value into Biginteger
+	/**
+	 * This Method is used to convert String into BigIngteger
+	 * @return BigInteger
+	 * @param String
+	 * 
+	 * */
 	public BigInteger bigIntegerRoundStr(String in){
 
 		BigInteger bigStr=null;
@@ -56,7 +78,25 @@ public final class IndianCurrencyHelper {
 		}else
 			return null;
 	}
-	
-	
+
+	/**
+	 * This Method is used to GregorianCalend into XMLGregorianCalendar
+	 * @return GregorianCalendar
+	 * @param Calendar
+	 * 
+	 * */
+	public XMLGregorianCalendar gregorianCalendar(GregorianCalendar val_Date){
+
+		XMLGregorianCalendar date2=null;
+		try {
+			//date2 = DatatypeFactory.newInstance().newXMLGregorianCalendar(dobStr);
+			date2 = DatatypeFactory.newInstance().newXMLGregorianCalendar(val_Date);
+
+		} catch (DatatypeConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return date2;
+	}
 
 }
