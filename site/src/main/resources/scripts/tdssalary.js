3c3d4cb631f3e9eea12e4f2b2628e2ab9032d0bf
Priyank Gupta <pgupta@mootly.com>
2013-05-28 05:42:06 +0000

    calculation in bind

--- site/src/main/resources/scripts/tdssalary.js
+++ site/src/main/resources/scripts/tdssalary.js
@@ -3,7 +3,7 @@ var sumTdsSalary=0;
 if(tdssalary!=null){
 	if (tdssalary.getTdsSalaryDetailList() != null &&  tdssalary.getTdsSalaryDetailList().size() > 0){ 	
 		for(var i=0;i<tdssalary.getTdsSalaryDetailList().size();i++) 
-			sumTdsSalary=sumTdsSalary+ tdssalary.getTdsSalaryDetailList().get(i).getP_Amount();			
+			sumTdsSalary=sumTdsSalary+ tdssalary.getTdsSalaryDetailList().get(i).getTotal_TaxDeducted();			
 	}
 }
 
