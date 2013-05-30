c2a2c4745e9661623e161bd549c943ce1f6cba3c
Dhananjay <dpanwar@mootly.com>
2013-05-30 08:41:33 +0000

    changes made for interest calc

--- site/src/main/webapp/js/interestcalculation/intt234.js
+++ site/src/main/webapp/js/interestcalculation/intt234.js
@@ -1,55 +1,56 @@
 
 function int234(){
-	var aytaxmp=$('aytaxmp').value;
-	$('aysfall').value=$('aytaxd').value*1-$('aytaxp').value*1;
-	if($('aysfall').value<0){
-		$('aysfall').value=0;
+	var aytaxmp=$('#aytaxmp').val();
+	//var aysfall = $('#aytaxd').val()-$('#aytaxp').val();
+	$('#aysfall').val($('#aytaxd').val()-$('#aytaxp').val());
+	if($('#aysfall').val()<0){
+		$('#aysfall').val(0);
 	}
-	if($('ddate').value==7&&$('aytaxmp').value>7){
-		$('dateA').value=aytaxmp*1-7*1;
+	if($('#ddate').val()==7&&$('#aytaxmp').val()>7){
+		$('#dateA').val(aytaxmp*1-7*1);
 	}else
-		if($('ddate').value==9&&$('aytaxmp').value>9){
-			$('dateA').value=aytaxmp*1-9*1;
+		if($('#ddate').val()==9&&$('#aytaxmp').val()>9){
+			$('#dateA').val(aytaxmp*1-9*1);
 		}else {
-			$('dateA').value=0;
+			$('#dateA').val(0);
 		}
-	$('dateB').value= $('aytaxmp').value*1-3*1;
-	if( $('dateB').value<0){
-		$('dateB').value=0;
+	$('#dateB').val($('#aytaxmp').val()*1-3*1) ;
+	if( $('#dateB').val()<0){
+		$('#dateB').val(0);
 	}
-	$('intA').value=Math.round($('aysfall').value * $('dateA').value /100);
-	$('intB').value=Math.round($('aysfall').value * $('dateB').value /100);
-	$('atdq2').value=Math.round($('aytaxd').value*30/100);
-	$('atsfq2').value=$('atdq2').value*1-$('atlq2').value *1-$('atpq2').value*1;
-	if($('atsfq2').value<0){
-		$('atsfq2').value=0;
+	$('#intA').val(Math.round($('#aysfall').val() * $('#dateA').val() /100));
+	$('#intB').val(Math.round($('#aysfall').val() * $('#dateB').val() /100));
+	$('#atdq2').val(Math.round($('#aytaxd').val()*30/100));
+	$('#atsfq2').val($('#atdq2').val()*1-$('#atlq2').val() *1-$('#atpq2').val()*1);
+	if($('#atsfq2').val()<0){
+		$('#atsfq2').val(0);
 	}
-	if($('aytaxd').value>=10000){
-		$('atiq2').value=Math.round($('atsfq2').value*3/100);
+	if($('#aytaxd').val()>=10000){
+		$('#atiq2').val(Math.round($('#atsfq2').val()*3/100));
 	}else{
-		$('atiq2').value=0;
+		$('#atiq2').val(0);
 	}
-	$('atdq3').value=Math.round($('aytaxd').value*60/100);
-	$('atsfq3').value=$('atdq3').value*1-$('atlq3').value*1-$('atpq3').value*1;
-	if($('atsfq3').value<0){
-		$('atsfq3').value=0;
+	$('#atdq3').val(Math.round($('#aytaxd').val()*60/100));
+	$('#atsfq3').val($('#atdq3').val()*1-$('#atlq3').val()*1-$('#atpq3').val()*1);
+	if($('#atsfq3').val()<0){
+		$('#atsfq3').val(0);
 	}
-	if($('aytaxd').value>=10000){
-		$('atiq3').value=Math.round($('atsfq3').value*3/100);
+	if($('#aytaxd').val()>=10000){
+		$('#atiq3').val(Math.round($('#atsfq3').val()*3/100));
 	}else{
-		$('atiq3').value=0;
+		$('#atiq3').val(0);
 	}
-	$('atdq4').value=Math.round($('aytaxd').value);
-	$('atsfq4').value=$('atdq4').value*1-$('atlq4').value*1-$('atpq4').value*1;
-	if($('atsfq4').value<0){
-		$('atsfq4').value=0;
+	$('#atdq4').val(Math.round($('#aytaxd').val()));
+	$('#atsfq4').val($('#atdq4').val()*1-$('#atlq4').val()*1-$('#atpq4').val()*1);
+	if($('#atsfq4').val()<0){
+		$('#atsfq4').val(0);
 	}
-	if($('aytaxd').value>=10000){
-		$('atiq4').value=Math.round($('atsfq4').value/100);
+	if($('#aytaxd').val()>=10000){
+		$('#atiq4').val(Math.round($('#atsfq4').val()/100));
 	}else{
-		$('atiq4').value=0;
+		$('#atiq4').val(0);
 	}
-	$('ic').value= $('atiq2').value*1+$('atiq3').value*1+$('atiq4').value*1;
-	$('intt').value=$('ic').value*1+$('intA').value*1+$('intB').value*1;
+	$('#ic').val($('#atiq2').val()*1+$('#atiq3').val()*1+$('#atiq4').val()*1);
+	$('#intt').val($('#ic').val()*1+$('#intA').val()*1+$('#intB').val()*1);
 }
 window.onload=int234();
