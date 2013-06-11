701b73e316d1ee8b182e8ea5cfe64d23d486ebd1
Pankaj <psingh@mootly.com>
2013-06-11 17:56:02 +0000

    modification for nea field sec 89

--- site/src/main/java/com/mootly/wcm/member/RebateSec90.java
+++ site/src/main/java/com/mootly/wcm/member/RebateSec90.java
@@ -1,7 +1,10 @@
 package com.mootly.wcm.member;
+import org.apache.commons.logging.Log;
 import org.hippoecm.hst.core.component.HstComponentException;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.request.ResolvedSiteMapItem;
+
 import com.mootly.wcm.annotations.AdditionalBeans;
 import com.mootly.wcm.annotations.FormFields;
 import com.mootly.wcm.annotations.PrimaryBean;
@@ -12,6 +15,11 @@ import com.mootly.wcm.beans.OtherSourcesDocument;
 import com.mootly.wcm.beans.RebateSec90Document;
 import com.mootly.wcm.beans.SalaryIncomeDocument;
 import com.mootly.wcm.components.ITReturnComponent;
+import com.mootly.wcm.utils.GoGreenUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.hippoecm.hst.core.request.ResolvedSiteMapItem;
 
 /**
 
@@ -21,7 +29,7 @@ import com.mootly.wcm.components.ITReturnComponent;
  *
  */
 @PrimaryBean(primaryBeanClass=RebateSec90Document.class)
-@FormFields(fieldNames= {"userCountry","taxPaidForeignCountry","incomeForeignCountry","txttotaltax","section91"})
+@FormFields(fieldNames= {"userCountry","taxPaidForeignCountry","incomeForeignCountry","txttotaltax","section91","section89"})
 @AdditionalBeans(additionalBeansToLoad={MemberPersonalInformation.class,SalaryIncomeDocument.class,HouseProperty.class,OtherSourcesDocument.class})
 @RequiredBeans(requiredBeans={MemberPersonalInformation.class})
 
@@ -32,9 +40,11 @@ public class RebateSec90 extends ITReturnComponent {
 	@Override
 	public void doBeforeRender(HstRequest request, HstResponse response) {
 		// TODO Auto-generated method stub
+		
 		super.doBeforeRender(request, response);
 		// to find eithr user lies in section 90 or 91
-	
+		//String isSection91=getParameter("isSection91", request);
+		
 	}
 	
 	@Override
@@ -43,6 +53,7 @@ public class RebateSec90 extends ITReturnComponent {
 		
 		// TODO Auto-generated method stub
 		super.doAction(request, response);
+		
 		
 	}
 	
