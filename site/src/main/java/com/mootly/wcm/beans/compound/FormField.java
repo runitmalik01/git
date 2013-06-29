3122bfdb4d3b6ced60a69ca5fa5c83962949e90d
Priyank Gupta <pgupta@mootly.com>
2013-06-29 07:12:42 +0000

    added services

---
+++ site/src/main/java/com/mootly/wcm/beans/compound/FormField.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2010 Hippo B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mootly.wcm.beans.compound;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoItem;
+
+/**
+ * Bean mapping class for the 'mootlywcm:address' document type
+ */
+@Node(jcrType = "mootlywcm:formField")
+public class FormField extends HippoItem {
+    public String getName() {
+        return getProperty("mootlywcm:name");
+    }
+
+    public String getFieldType() {
+        return getProperty("mootlywcm:fieldType");
+    }
+
+    public String getClassName() {
+        return getProperty("mootlywcm:class");
+    }
+
+    public String getInputType() {
+        return getProperty("mootlywcm:inputType");
+    }
+
+    public String[] getDropDownOptions() {
+        return getProperty("mootlywcm:option");
+    }
+
+}
