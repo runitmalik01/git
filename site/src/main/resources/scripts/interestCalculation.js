466d11fe9e231c727f5efca4a25d2b78dcc56f5b
Dhananjay <dpanwar@mootly.com>
2013-07-12 06:24:40 +0000

    trying to reduce difference of rupees

--- site/src/main/resources/scripts/interestCalculation.js
+++ site/src/main/resources/scripts/interestCalculation.js
@@ -5,21 +5,21 @@ if(aysfall<0){
 	aysfall = 0;
 }
 if(ddate==7 && aytaxmp>7){
-	var dateA = aytaxmp*1-7*1;
+	var dateA = aytaxmp-7;
 }else
 	if(ddate==9 && aytaxmp>9){
-		var dateA = aytaxmp*1-9*1;
+		var dateA = aytaxmp-9;
 	}else {
 		var dateA = 0;
 	}
-var dateB = aytaxmp*1-3*1 ;
+var dateB = aytaxmp-3;
 if( dateB<0){
 	dateB = 0 ;
 }
 out_intA = Math.round(aysfall * dateA/100);
 out_intB = Math.round(aysfall * dateB/100);
 var atdq2 = Math.round(aytaxd*30/100);
-var atsfq2 = atdq2*1-atlq2*1-atpq2*1;
+var atsfq2 = atdq2-atlq2-atpq2;
 if(atsfq2 < 0){
 	atsfq2 = 0;
 }
@@ -29,7 +29,7 @@ if(aytaxd>=10000){
 	var atiq2 = 0;
 }
 var atdq3 = Math.round(aytaxd*60/100);
-var atsfq3 = atdq3*1-atlq3*1-atpq3*1;
+var atsfq3 = atdq3-atlq3-atpq3;
 if(atsfq3<0){
 	atsfq3 = 0;
 }
@@ -39,7 +39,7 @@ if(aytaxd>=10000){
 	var atiq3 = 0;
 }
 atdq4 = Math.round(aytaxd);
-var atsfq4 = atdq4*1-atlq4*1-atpq4*1;
+var atsfq4 = atdq4-atlq4-atpq4;
 if(atsfq4 < 0){
 	atsfq4 = 0;
 }
@@ -48,5 +48,5 @@ if(aytaxd>=10000){
 }else{
 	var atiq4 = 0;
 }
-out_ic = atiq2*1+atiq3*1+atiq4*1;
-out_intt = out_ic*1+out_intA*1+out_intB*1;
+out_ic = atiq2+atiq3+atiq4;
+out_intt = out_ic+out_intA+out_intB;
