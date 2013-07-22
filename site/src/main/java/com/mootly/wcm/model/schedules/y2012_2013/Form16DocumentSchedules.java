43f34de86a3c7929a9ffecf2d403e918d82016a6
Abhishek <abhardwaj@mootly.com>
2013-07-22 06:41:54 +0000

    changes made for pension and salary schedules

--- site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/Form16DocumentSchedules.java
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/Form16DocumentSchedules.java
@@ -11,6 +11,7 @@ import java.util.List;
 import com.mootly.wcm.beans.FormSixteenDocument;
 import com.mootly.wcm.beans.SalaryIncomeDocument;
 import com.mootly.wcm.beans.compound.FormSixteenDetail;
+import com.mootly.wcm.beans.compound.SalaryIncomeDetail;
 import com.mootly.wcm.services.IndianCurrencyHelper;
 
 public class Form16DocumentSchedules {
@@ -32,12 +33,16 @@ public class Form16DocumentSchedules {
 		IndianCurrencyHelper indianCurrencyHelper = new IndianCurrencyHelper();
 		ScheduleS scheduleS = new ScheduleS();
 		List<FormSixteenDetail> formSixteenDetails = formSixteenDocument.getFormSixteenDetailList();
+		List<SalaryIncomeDetail> salaryIncomeDetails = salaryIncomeDocument.getSalaryIncomeDetailList();
 		for (FormSixteenDetail formSixteenDetail:formSixteenDetails)  {
 			Salaries salaries = new Salaries();
 			
 			AddressDetail addressDetail = new AddressDetail();
 			addressDetail.setAddrDetail(formSixteenDetail.getAddress());
+			addressDetail.setAddrDetail(formSixteenDetail.getEmploye_category());
+			addressDetail.setAddrDetail(formSixteenDetail.getEmployee());
 			salaries.setNameOfEmployer(formSixteenDetail.getEmployer());
+			salaries.setPANofEmployer(formSixteenDetail.getPan_deductor());
 			Salarys salarys = new Salarys();
 			salarys.setSalary(indianCurrencyHelper.bigIntegerRound( formSixteenDetail.getGross_a() ));
 			salarys.setValueOfPerquisites(indianCurrencyHelper.bigIntegerRound( formSixteenDetail.getGross_b()) );
@@ -46,6 +51,27 @@ public class Form16DocumentSchedules {
 			
 			salarys.setSalary(indianCurrencyHelper.bigIntegerRound( formSixteenDetail.getIncome_chargable_tax()));
 			
+			scheduleS.getSalaries().add(salaries);
+		}
+		
+		/*
+		 * this loop will take care of pension income
+		 * and related address details
+		 */
+		for (SalaryIncomeDetail salaryIncomeDetail:salaryIncomeDetails)  {
+			Salaries salaries = new Salaries();
+			
+			AddressDetail addressDetail = new AddressDetail();
+			addressDetail.setAddrDetail(salaryIncomeDetail.getAddress());
+			addressDetail.setAddrDetail(salaryIncomeDetail.getEmploye_category());
+			addressDetail.setAddrDetail(salaryIncomeDetail.getCity());
+			addressDetail.setAddrDetail(salaryIncomeDetail.getState());
+			addressDetail.setAddrDetail(salaryIncomeDetail.getPin());
+			salaries.setNameOfEmployer(salaryIncomeDetail.getName_employer());
+			salaries.setPANofEmployer(salaryIncomeDetail.getPan_employer());
+			Salarys salarys = new Salarys();
+			salarys.setSalary(indianCurrencyHelper.bigIntegerRound( salaryIncomeDetail.getGross_salary() ));
+			salarys.setValueOfPerquisites(indianCurrencyHelper.bigIntegerRound( salaryIncomeDetail.getTdsPension()) );
 			scheduleS.getSalaries().add(salaries);
 		}
 		return scheduleS;
