71d92f1e06bbb8dc75bd298aa8460a7684daf410
Dhananjay <dpanwar@mootly.com>
2013-07-26 11:28:23 +0000

    added XmlCalculationImplement.java

---
+++ site/src/main/java/com/mootly/wcm/utils/XmlCalculationImplement.java
@@ -0,0 +1,10 @@
+package com.mootly.wcm.utils;
+
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+
+public interface XmlCalculationImplement {
+
+	public long grossTotal(HstRequest request,HstResponse response);
+
+}
