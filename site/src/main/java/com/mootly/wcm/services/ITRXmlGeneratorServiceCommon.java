7233fc0d7de13403eaa755c132925099a33a3178
Dhananjay <dpanwar@mootly.com>
2013-07-31 17:14:32 +0000

    added new method for due date in ITRXmlGeneratorServiceCommon.java

--- site/src/main/java/com/mootly/wcm/services/ITRXmlGeneratorServiceCommon.java
+++ site/src/main/java/com/mootly/wcm/services/ITRXmlGeneratorServiceCommon.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
+import org.apache.james.mime4j.field.datetime.DateTime;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
@@ -26,7 +27,7 @@ public class ITRXmlGeneratorServiceCommon implements XmlGeneratorService {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public ValidationResponse validateXml(String xml) throws Exception {
 		// TODO Auto-generated method stub
@@ -60,7 +61,7 @@ public class ITRXmlGeneratorServiceCommon implements XmlGeneratorService {
 	public static String getIntermediaryCity() {
 		return "New Delhi";
 	}
-	
+
 	public static String getSWCreatedBy() {
 		return "Wealth4India";
 	}
@@ -68,32 +69,32 @@ public class ITRXmlGeneratorServiceCommon implements XmlGeneratorService {
 	public static String getXMLCreatedBy() {
 		return "Wealth4India";
 	}
-	
-	
+
+
 	public static String getCurrentDateInIndiaAsString() {
 		Calendar currentdate = Calendar.getInstance();
-        String strdate = null;
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        strdate = formatter.format(currentdate.getTime());
-        System.out.println("strdate=>" + strdate);
-        TimeZone obj = TimeZone.getTimeZone("GMT + 5:30");
+		String strdate = null;
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		strdate = formatter.format(currentdate.getTime());
+		System.out.println("strdate=>" + strdate);
+		TimeZone obj = TimeZone.getTimeZone("GMT + 5:30");
 
-        formatter.setTimeZone(obj);
-        String strDate = formatter.format(currentdate.getTime());
-        return strDate;
+		formatter.setTimeZone(obj);
+		String strDate = formatter.format(currentdate.getTime());
+		return strDate;
 	}
-	
+
 	public static GregorianCalendar getCurrentDateInIndiaAsDate() {
 		Calendar currentdate =  GregorianCalendar.getInstance();
-        String strdate = null;
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        strdate = formatter.format(currentdate.getTime());
-        System.out.println("strdate=>" + strdate);
-        TimeZone obj = TimeZone.getTimeZone("GMT+5:30");
+		String strdate = null;
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		strdate = formatter.format(currentdate.getTime());
+		System.out.println("strdate=>" + strdate);
+		TimeZone obj = TimeZone.getTimeZone("GMT+5:30");
 
-        formatter.setTimeZone(obj);
-        String strDate = formatter.format(currentdate.getTime());
-        try {
+		formatter.setTimeZone(obj);
+		String strDate = formatter.format(currentdate.getTime());
+		try {
 			Date theResult = formatter.parse(strDate);
 			GregorianCalendar theNewOne = new GregorianCalendar();
 			theNewOne.setTimeZone(obj);
@@ -106,9 +107,25 @@ public class ITRXmlGeneratorServiceCommon implements XmlGeneratorService {
 			theNewOne.setTimeZone(obj);
 			theNewOne.setTime(currentdate.getTime());
 			return theNewOne;
-		}        
+		}
 	}
-	
+
+	/**
+	 * This Method is used to compare due date and current date
+	 * @return boolean
+	 * Added on 31/07/2013 by Dhananjay
+	 * */
+
+	public static boolean getDueDate(){
+		boolean isDueDate = false;
+		String currentDate = getCurrentDateInIndiaAsString();
+		String dueDate = "2013-08-05";
+		if (currentDate.compareTo(dueDate)<0 || currentDate.compareTo(dueDate)==0){
+			isDueDate = true;
+		}
+		return isDueDate;
+	}
+
 	public static void main(String[] args) {
 		ITRXmlGeneratorServiceCommon c = new ITRXmlGeneratorServiceCommon();
 		String theString = c.getCurrentDateInIndiaAsString();
