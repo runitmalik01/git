e23109b29ce99630ee2e3fc78813fe6b51e34e0e
Pankaj <psingh@mootly.com>
2013-08-06 10:35:41 +0000

    added component for schedule 5a

---
+++ site/src/main/java/com/mootly/wcm/member/Schedule5A.java
@@ -0,0 +1,46 @@
+package com.mootly.wcm.member;
+
+
+import org.hippoecm.hst.core.component.HstComponentException;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mootly.wcm.annotations.FormFields;
+import com.mootly.wcm.annotations.PrimaryBean;
+import com.mootly.wcm.beans.ScheduleFiveADocument;
+import com.mootly.wcm.components.ITReturnComponent;
+
+/*
+ * Author:Pankaj Singh
+ * Date:13/3/2013
+ * Description:It will taje value from Tdsfromsalary.jsp and pass it to bean
+ */
+@PrimaryBean(primaryBeanClass=ScheduleFiveADocument.class)
+@FormFields(fieldNames={"name_spouse","pan_spouse","capital_gains","other_sources","house_property"})
+public class Schedule5A extends ITReturnComponent {
+
+	private static final Logger log = LoggerFactory.getLogger(TdsFromSalary.class);
+	public void doBeforeRender(HstRequest request, HstResponse response) {
+		// TODO Auto-generated method stub
+		super.doBeforeRender(request, response);
+		if(log.isInfoEnabled()){
+			log.info("this is do before render of schedule5a screen");
+		}
+	}
+	@Override
+	public void doAction(HstRequest request, HstResponse response)
+			throws HstComponentException {
+
+		// TODO Auto-generated method stub
+		super.doAction(request, response);
+		if(log.isInfoEnabled()){
+			log.info("this is do Action of schedule 5a screen");
+		}
+
+	} }
+
+
+
+
