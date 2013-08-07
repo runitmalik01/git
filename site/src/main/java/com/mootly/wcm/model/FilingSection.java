a2704b32e2d77cb070dc2d8525a137f24b847804
Dhananjay <dpanwar@mootly.com>
2013-08-07 05:51:45 +0000

    changes made for revised and original changes in FilingSection.java

--- site/src/main/java/com/mootly/wcm/model/FilingSection.java
+++ site/src/main/java/com/mootly/wcm/model/FilingSection.java
@@ -6,27 +6,27 @@ import java.util.List;
 public enum FilingSection {
 	BeforeDueDate_139_1("11","Before Due Date","original",ITReturnType.ORIGINAL,false),
 	AfterDueDate_139_4("12","After Due Date","139_4",ITReturnType.ORIGINAL,false),
-	u_s_142_1("13","u/s 142(1)","u_s_142_1",ITReturnType.REVISED,true),
-	u_s_148("14","u/s 148","u_s_148",ITReturnType.REVISED,true),
-	u_s_153A("15","u/s 153A","u_s_153A",ITReturnType.REVISED,true),
-	u_s_153C_r_w_153A("16","u/s 153 r/w 153A","u_s_153C_r_w_153A",ITReturnType.REVISED,true),
+	u_s_142_1("13","u/s 142(1)","u_s_142_1",ITReturnType.ORIGINAL,true),
+	u_s_148("14","u/s 148","u_s_148",ITReturnType.ORIGINAL,true),
+	u_s_153A("15","u/s 153A","u_s_153A",ITReturnType.ORIGINAL,true),
+	u_s_153C_r_w_153A("16","u/s 153 r/w 153A","u_s_153C_r_w_153A",ITReturnType.ORIGINAL,true),
 	Revised_139_5("17","Revised 139(5)","revised",ITReturnType.REVISED,false),
-	Revised_139_9("18","u/s 139(9)","revised_defective",ITReturnType.REVISED,true),  //DEFECIVE RETURN
+	Revised_139_9("18","u/s 139(9) (Defective)","revised_defective",ITReturnType.ORIGINAL,true),  //DEFECIVE RETURN
 	UNKNOWN;
-	
+
 	String xmlCode;
 	String desc;
 	String folderName;
 	ITReturnType itReturnType;
 	boolean requiresNotice;
-	
+
 	private FilingSection() {
 	}
-	
+
 	private FilingSection(String xmlCode) {
 		this.xmlCode = xmlCode;
 	}
-	
+
 	private FilingSection(String xmlCode,String desc,String folderName,ITReturnType itReturnType,boolean requiresNotice) {
 		this.xmlCode = xmlCode;
 		this.desc = desc;
@@ -34,7 +34,7 @@ public enum FilingSection {
 		this.itReturnType = itReturnType;
 		this.requiresNotice = requiresNotice;
 	}
-	
+
 	public String getdisplayString() {
 		if (desc == null) {
 			return toString();
@@ -46,7 +46,7 @@ public enum FilingSection {
 	public String getXmlCode(){
 		return xmlCode;
 	}
-	
+
 	public String getDesc() {
 		return desc;
 	}
@@ -54,11 +54,11 @@ public enum FilingSection {
 	public String getFolderName() {
 		return folderName;
 	}
-	
+
 	public ITReturnType getItReturnType() {
 		return itReturnType;
 	}
-	
+
 	public boolean isRequiresNotice() {
 		return requiresNotice;
 	}
@@ -76,5 +76,5 @@ public enum FilingSection {
 		}
 		return UNKNOWN;
 	}
-	
+
 }
