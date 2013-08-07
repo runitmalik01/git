56330a72c031de4dbe28d60246fd0672ce5c5cea
Dhananjay <dpanwar@mootly.com>
2013-08-07 10:29:25 +0000

    added PartB_TTI.java

---
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/PartB_TTI.java
@@ -0,0 +1,16 @@
+package com.mootly.wcm.model.schedules.y2012_2013;
+
+import in.gov.incometaxindiaefiling.y2011_2012.ITR;
+import in.gov.incometaxindiaefiling.y2012_2013.PartBTTI;
+
+public class PartB_TTI {
+
+	public PartBTTI getPartBTTI(ITR itr){
+		PartBTTI partBTTI = new PartBTTI();
+
+
+
+
+		return partBTTI;
+	}
+}
