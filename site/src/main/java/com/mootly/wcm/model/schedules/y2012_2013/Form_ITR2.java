7748247a8050c9ace2623c59f2b33562f97ee7b0
Dhananjay <dpanwar@mootly.com>
2013-08-07 10:28:26 +0000

    added Form_ITR2.java

---
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/Form_ITR2.java
@@ -0,0 +1,18 @@
+package com.mootly.wcm.model.schedules.y2012_2013;
+
+import in.gov.incometaxindiaefiling.y2012_2013.FormITR2;
+import in.gov.incometaxindiaefiling.y2012_2013.ITR;
+
+public class Form_ITR2 {
+
+	public FormITR2 getFormITR2(ITR itr){
+
+		FormITR2 formITR2 = new FormITR2();
+		formITR2.setFormName("ITR-2");
+		formITR2.setDescription("For Indls and  HUFs not having Income from Business or Profession");
+		formITR2.setAssessmentYear("2013");
+		formITR2.setSchemaVer("Ver1.0");
+		formITR2.setFormVer("Ver1.0");
+		return formITR2;
+	}
+}
