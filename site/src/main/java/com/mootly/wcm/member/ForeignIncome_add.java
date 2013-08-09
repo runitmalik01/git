00fee4e5f63fd6126dd246301aa2dfe63e95672a
Pankaj <psingh@mootly.com>
2013-08-09 07:59:28 +0000

    added new java to be used with foreignincome screen...

---
+++ site/src/main/java/com/mootly/wcm/member/ForeignIncome_add.java
@@ -0,0 +1,58 @@
+package com.mootly.wcm.member;
+
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.hippoecm.hst.content.beans.ObjectBeanManagerException;
+import org.hippoecm.hst.content.beans.manager.workflow.WorkflowPersistenceManager;
+import org.hippoecm.hst.core.component.HstComponentException;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mootly.wcm.annotations.FormFields;
+import com.mootly.wcm.annotations.PrimaryBean;
+import com.mootly.wcm.beans.ForeignIncomeDocument;
+import com.mootly.wcm.beans.ScheduleFiveADocument;
+import com.mootly.wcm.components.ITReturnComponent;
+import com.mootly.wcm.components.InvalidNavigationException;
+
+/*
+ * Author:Pankaj Singh
+ * 
+ */
+@PrimaryBean(primaryBeanClass=ForeignIncomeDocument.class)
+@FormFields(fieldNames={"IncomeApplDtaa"})
+
+
+public class ForeignIncome_add extends ITReturnComponent {
+
+	private static final Logger log = LoggerFactory.getLogger(TdsFromSalary.class);
+	public void doBeforeRender(HstRequest request, HstResponse response) {
+		// TODO Auto-generated method stub
+		super.doBeforeRender(request, response);
+		if(log.isInfoEnabled()){
+			log.info("this is do before render of additional foreignincome screen");
+		}
+		
+	}
+	@Override
+	public void doAction(HstRequest request, HstResponse response)
+			throws HstComponentException {
+
+		// TODO Auto-generated method stub
+		super.doAction(request, response);
+		if(log.isInfoEnabled()){
+			log.info("this is do Action of additional foreignincome screen");
+		}
+	
+	
+		}
+
+	} 
+
+
+
+
