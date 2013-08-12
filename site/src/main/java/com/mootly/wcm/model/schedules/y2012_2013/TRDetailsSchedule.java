b3f2f88b6ad7ed5fca3c6c441a99078dab98c8b5
Dhananjay <dpanwar@mootly.com>
2013-08-12 13:01:56 +0000

    changes made for TRDetailsSchedule.java

--- site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/TRDetailsSchedule.java
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/TRDetailsSchedule.java
@@ -14,32 +14,42 @@ import com.mootly.wcm.services.IndianCurrencyHelper;
 
 public class TRDetailsSchedule {
 	TaxReliefDocument taxReliefDocument = null;
-	
+
 	public TRDetailsSchedule (TaxReliefDocument taxRelief){
 		this.taxReliefDocument=taxRelief;
-		
+
 	}
-	public ScheduleTR1 scheduleTR1(ITR itr){
+	public ScheduleTR1 getScheduleTR1(ITR itr){
 		IndianCurrencyHelper indianCurrencyHelper = new IndianCurrencyHelper();
 		ScheduleTR1 scheduleTR1 = new ScheduleTR1();
-		List<TaxReliefDetail> taxReliefDetails = taxReliefDocument.getTaxReliefDetailList();
-		for(TaxReliefDetail taxReliefDetail:taxReliefDetails){
-			ScheduleTR scheduleTR = new ScheduleTR();
-			scheduleTR.setCountryCode(taxReliefDetail.getCountry_Code());
-			scheduleTR.setCountryName(taxReliefDetail.getCountry_Name());
-			scheduleTR.setTaxIdentificationNo(taxReliefDetail.getTax_ID());
-			scheduleTR.setRelavantArticleDTAA(taxReliefDetail.getArticle_dtaa());
-			scheduleTR.setTotalTaxpaid(indianCurrencyHelper.bigIntegerRound(taxReliefDetail.getTotaltax_fsi()));
-			TotTaxreliefClaimed totTaxreliefClaimed= new TotTaxreliefClaimed();
-			totTaxreliefClaimed.setReliefClaimed90Or90A(indianCurrencyHelper.bigIntegerRound(taxReliefDetail.getRelief90_91()));
-			totTaxreliefClaimed.setReliefClaimedUs91(indianCurrencyHelper.bigIntegerRound(taxReliefDetail.getRelief91()));
-			scheduleTR.setTotTaxreliefClaimed(totTaxreliefClaimed);
-			
-			scheduleTR1.getScheduleTR().add(scheduleTR);
-			scheduleTR1.setTaxPaidOutsideIndiaDTAA(indianCurrencyHelper.bigIntegerRound(taxReliefDocument.getTaxPaidDtaa()));
-			scheduleTR1.setTaxPaidOutsideIndiaNotDTAA(indianCurrencyHelper.bigIntegerRound(taxReliefDocument.getTaxPaidNoDtaa()));
+		boolean hasValidTR = false;
+		if(taxReliefDocument!=null){
+			List<TaxReliefDetail> taxReliefDetails = taxReliefDocument.getTaxReliefDetailList();
+			if ( taxReliefDetails != null && taxReliefDetails.size() > 0 ){
+				for(TaxReliefDetail taxReliefDetail:taxReliefDetails){
+					ScheduleTR scheduleTR = new ScheduleTR();
+					scheduleTR.setCountryCode(taxReliefDetail.getCountry_Code());
+					scheduleTR.setCountryName(taxReliefDetail.getCountry_Name());
+					scheduleTR.setTaxIdentificationNo(taxReliefDetail.getTax_ID());
+					scheduleTR.setRelavantArticleDTAA(taxReliefDetail.getArticle_dtaa());
+					scheduleTR.setTotalTaxpaid(indianCurrencyHelper.bigIntegerRound(taxReliefDetail.getTotaltax_fsi()));
+					TotTaxreliefClaimed totTaxreliefClaimed= new TotTaxreliefClaimed();
+					totTaxreliefClaimed.setReliefClaimed90Or90A(indianCurrencyHelper.bigIntegerRound(taxReliefDetail.getRelief90_91()));
+					totTaxreliefClaimed.setReliefClaimedUs91(indianCurrencyHelper.bigIntegerRound(taxReliefDetail.getRelief91()));
+					scheduleTR.setTotTaxreliefClaimed(totTaxreliefClaimed);
+
+					scheduleTR1.getScheduleTR().add(scheduleTR);
+					scheduleTR1.setTaxPaidOutsideIndiaDTAA(indianCurrencyHelper.bigIntegerRound(taxReliefDocument.getTaxPaidDtaa()));
+					scheduleTR1.setTaxPaidOutsideIndiaNotDTAA(indianCurrencyHelper.bigIntegerRound(taxReliefDocument.getTaxPaidNoDtaa()));
+					scheduleTR1.setTotalTaxPaidOutsideIndia(indianCurrencyHelper.bigIntegerRound(taxReliefDocument.getTotal_TaxFsi()));
+					if(!hasValidTR) hasValidTR = true;
+				}
+			}
 		}
-		return scheduleTR1;
+		if(hasValidTR){
+			return scheduleTR1;
+		}else
+			return null;
 	}
-    
+
 }
