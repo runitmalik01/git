4b0c164f791b0db871901dfa6f7097f1d22fb4f1
Dhananjay <dpanwar@mootly.com>
2013-08-12 13:01:01 +0000

    changes made for ForeignIncomeScheduleFSI.java

--- site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ForeignIncomeScheduleFSI.java
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ForeignIncomeScheduleFSI.java
@@ -12,35 +12,43 @@ import com.mootly.wcm.services.IndianCurrencyHelper;
 
 public class ForeignIncomeScheduleFSI {
 	ForeignIncomeDocument foreignIncomeDoc = null;
-	
+
 	public ForeignIncomeScheduleFSI(ForeignIncomeDocument ForeignIncome){
 		this.foreignIncomeDoc = ForeignIncome;
-		
+
 	}
 	public ITRScheduleFSI getITRScheduleFSI (ITR itr){
 		IndianCurrencyHelper indianCurrencyHelper = new IndianCurrencyHelper();
 		ITRScheduleFSI iTRScheduleFSI = new ITRScheduleFSI();
-		List<ForeignIncomeDetail> foreignIncomeDetail =  foreignIncomeDoc.getForeignIncomeDetailList();
-		for(ForeignIncomeDetail foreignIncomedet:foreignIncomeDetail){
-			ScheduleFSI scheduleFSI = new ScheduleFSI();
-			scheduleFSI.setCountryCode(foreignIncomedet.getCountry_Code());
-			scheduleFSI.setCountryName(foreignIncomedet.getCountry_Name());
-			scheduleFSI.setTaxIdentificationNo(foreignIncomedet.getTaxpayer_ID());
-			scheduleFSI.setIncFromSal(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Salary()));
-			scheduleFSI.setIncFromHP(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_House()));
-			scheduleFSI.setIncFromBusiness(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Business()));
-			scheduleFSI.setIncCapGain(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Capitalgain()));
-			scheduleFSI.setIncOthSrc(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Othersources()));
-			scheduleFSI.setTotalCountryWise(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Total()));
-	
-			
-			iTRScheduleFSI.getScheduleFSI().add(scheduleFSI);
-			iTRScheduleFSI.setTotalIncomeOutIndia(indianCurrencyHelper.bigIntegerRound(foreignIncomeDoc.getTotal_Amount()));
-			iTRScheduleFSI.setTotalIncomeOutIndiaDTAA(indianCurrencyHelper.bigIntegerRound(foreignIncomeDoc.getIncomeApplDtaa()));
-			iTRScheduleFSI.setTotalIncomeOutIndiaWoDTAA(indianCurrencyHelper.bigIntegerRound(foreignIncomeDoc.getIncomeNotApplDtaa()));
+		boolean hasValidForeignIncome = false;
+		if(foreignIncomeDoc!=null){
+			List<ForeignIncomeDetail> foreignIncomeDetail =  foreignIncomeDoc.getForeignIncomeDetailList();
+			if ( foreignIncomeDetail != null && foreignIncomeDetail.size() > 0 ){
+				for(ForeignIncomeDetail foreignIncomedet:foreignIncomeDetail){
+					ScheduleFSI scheduleFSI = new ScheduleFSI();
+					scheduleFSI.setCountryCode(foreignIncomedet.getCountry_Code());
+					scheduleFSI.setCountryName(foreignIncomedet.getCountry_Name());
+					scheduleFSI.setTaxIdentificationNo(foreignIncomedet.getTaxpayer_ID());
+					scheduleFSI.setIncFromSal(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Salary()));
+					scheduleFSI.setIncFromHP(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_House()));
+					scheduleFSI.setIncFromBusiness(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Business()));
+					scheduleFSI.setIncCapGain(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Capitalgain()));
+					scheduleFSI.setIncOthSrc(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Othersources()));
+					scheduleFSI.setTotalCountryWise(indianCurrencyHelper.bigIntegerRound(foreignIncomedet.getIncome_Total()));
+
+
+					iTRScheduleFSI.getScheduleFSI().add(scheduleFSI);
+					iTRScheduleFSI.setTotalIncomeOutIndia(indianCurrencyHelper.bigIntegerRound(foreignIncomeDoc.getTotal_Amount()));
+					iTRScheduleFSI.setTotalIncomeOutIndiaDTAA(indianCurrencyHelper.bigIntegerRound(foreignIncomeDoc.getIncomeApplDtaa()));
+					iTRScheduleFSI.setTotalIncomeOutIndiaWoDTAA(indianCurrencyHelper.bigIntegerRound(foreignIncomeDoc.getIncomeNotApplDtaa()));
+					if(!hasValidForeignIncome) hasValidForeignIncome = true;
+				}
+			}
 		}
-		
-		return iTRScheduleFSI;
+		if(hasValidForeignIncome){
+			return iTRScheduleFSI;
+		}
+		return null;
 	}
 
 }
