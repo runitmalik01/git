c05bbe9a8e2d3297ce950e3e8cecc21ac60810bf
Pankaj <psingh@mootly.com>
2013-08-13 12:14:14 +0000

    complete working

--- site/src/main/java/com/mootly/wcm/member/Schedule5A.java
+++ site/src/main/java/com/mootly/wcm/member/Schedule5A.java
@@ -7,8 +7,11 @@ import org.hippoecm.hst.core.component.HstResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mootly.wcm.annotations.AdditionalBeans;
 import com.mootly.wcm.annotations.FormFields;
 import com.mootly.wcm.annotations.PrimaryBean;
+import com.mootly.wcm.annotations.RequiredBeans;
+import com.mootly.wcm.beans.MemberPersonalInformation;
 import com.mootly.wcm.beans.ScheduleFiveADocument;
 import com.mootly.wcm.components.ITReturnComponent;
 
@@ -19,6 +22,8 @@ import com.mootly.wcm.components.ITReturnComponent;
  */
 @PrimaryBean(primaryBeanClass=ScheduleFiveADocument.class)
 @FormFields(fieldNames={"name_spouse","pan_spouse","capital_gains","other_sources","house_property","total"})
+@RequiredBeans(requiredBeans=MemberPersonalInformation.class)
+
 public class Schedule5A extends ITReturnComponent {
 
 	private static final Logger log = LoggerFactory.getLogger(TdsFromSalary.class);
@@ -28,6 +33,10 @@ public class Schedule5A extends ITReturnComponent {
 		if(log.isInfoEnabled()){
 			log.info("this is do before render of schedule5a screen");
 		}
+		MemberPersonalInformation objMemberPersonalInformation=(MemberPersonalInformation) request.getAttribute(MemberPersonalInformation.class.getSimpleName().toLowerCase());
+		String isPortugese = objMemberPersonalInformation.getPortugesecivil();
+		if(isPortugese.equals("N")) request.setAttribute("isPortugese", "No");
+		else request.setAttribute("isPortugese", "Yes");
 	}
 	@Override
 	public void doAction(HstRequest request, HstResponse response)
