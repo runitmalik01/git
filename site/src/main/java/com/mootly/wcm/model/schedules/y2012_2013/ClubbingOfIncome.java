d9fd97648c8b2bb96004f741d8c3b1f838e6faa8
Dhananjay <dpanwar@mootly.com>
2013-08-16 18:46:15 +0000

    addded ClubbingOfIncome.java

---
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ClubbingOfIncome.java
@@ -0,0 +1,52 @@
+package com.mootly.wcm.model.schedules.y2012_2013;
+
+import in.gov.incometaxindiaefiling.y2012_2013.ITR;
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleEI;
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleSPI;
+import in.gov.incometaxindiaefiling.y2012_2013.SpecifiedPerson;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import com.mootly.wcm.beans.CapitalAssetDocument;
+import com.mootly.wcm.beans.ClubIncomeDocument;
+import com.mootly.wcm.beans.OtherSourcesDocument;
+import com.mootly.wcm.beans.compound.ClubIncomeDetail;
+import com.mootly.wcm.beans.compound.SalaryIncomeDetail;
+import com.mootly.wcm.services.IndianCurrencyHelper;
+
+public class ClubbingOfIncome {
+
+	ClubIncomeDocument clubIncomeDocument = null;
+
+	public ClubbingOfIncome(ClubIncomeDocument clubIncomeDocument){
+		this.clubIncomeDocument = clubIncomeDocument;
+	}
+
+	public ScheduleSPI getScheduleSPI(ITR itr){
+		IndianCurrencyHelper indianCurrencyHelper = new IndianCurrencyHelper();
+		ScheduleSPI scheduleSPI = new ScheduleSPI();
+		boolean hasAValidInc = false;
+		if(clubIncomeDocument != null){
+			List<ClubIncomeDetail> clubIncomeDetails = clubIncomeDocument.getClubIncomeDetailList();
+			if ( clubIncomeDetails != null && clubIncomeDetails.size() > 0 ){
+				for (ClubIncomeDetail ClubIncomeDetail:clubIncomeDetails)  {
+					SpecifiedPerson specifiedPerson = new SpecifiedPerson();
+					specifiedPerson.setSpecifiedPersonName(ClubIncomeDetail.getName_Person());
+					if(!(ClubIncomeDetail.getPan_person().isEmpty()))
+						specifiedPerson.setPANofSpecPerson(ClubIncomeDetail.getPan_person());
+					specifiedPerson.setReltnShip(ClubIncomeDetail.getRelationship());
+					specifiedPerson.setNatureOfInc(ClubIncomeDetail.getNature_income());
+					specifiedPerson.setAmtIncluded(indianCurrencyHelper.longRound(ClubIncomeDetail.getAmountclub()));
+					scheduleSPI.getSpecifiedPerson().add(specifiedPerson);
+					if(!hasAValidInc) hasAValidInc = true;
+				}
+			}
+		}
+		if(hasAValidInc){
+			return scheduleSPI;
+		}else
+			return null;
+
+	}
+}
