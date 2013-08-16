71f282ec20158269a89901a2631e1ececb249a30
Dhananjay <dpanwar@mootly.com>
2013-08-16 18:46:43 +0000

    changes made for ExemptIncomeSchedule.java

--- site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ExemptIncomeSchedule.java
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ExemptIncomeSchedule.java
@@ -1,7 +1,9 @@
 package com.mootly.wcm.model.schedules.y2012_2013;
 
+import java.math.BigInteger;
 import java.util.List;
 
+import in.gov.incometaxindiaefiling.y2012_2013.ITR;
 import in.gov.incometaxindiaefiling.y2012_2013.ScheduleEI;
 
 import org.omg.CORBA.PUBLIC_MEMBER;
@@ -14,29 +16,43 @@ import com.mootly.wcm.services.IndianCurrencyHelper;
 public class ExemptIncomeSchedule {
 	OtherSourcesDocument otherSourcesDocument = null;
 	CapitalAssetDocument capitalAssetDocument = null;
-	
+
 	public ExemptIncomeSchedule(OtherSourcesDocument otherSourcesDocument, CapitalAssetDocument capitalAssetDocument){
 		this.otherSourcesDocument=otherSourcesDocument;
 		this.capitalAssetDocument=capitalAssetDocument;
 	}
-	public ScheduleEI scheduleEI(){
+	public ScheduleEI getScheduleEI(ITR itr){
 		IndianCurrencyHelper indianCurrencyHelper = new IndianCurrencyHelper();
 		ScheduleEI scheduleEI = new ScheduleEI();
-		scheduleEI.setInterestInc(indianCurrencyHelper.bigIntegerRound(otherSourcesDocument.getBank_detail_fdr()));
-		scheduleEI.setDividendInc(indianCurrencyHelper.bigIntegerRound(otherSourcesDocument.getDividends()));
-		scheduleEI.setLTCGWhereSTTPaid(indianCurrencyHelper.bigIntegerRound(0d));
-		
-		/* commented for now.. waiting to complete capital gain module
-		 * List<CapitalAssetDetail> capitalAssetDetails = capitalAssetDocument.getCapitalAssetDetailList();
-		for(CapitalAssetDetail capitalAssetDetail:capitalAssetDetails){
-			System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"+indianCurrencyHelper.bigIntegerRoundStr(capitalAssetDetail.getCapitalGainTaxLT()));
-			scheduleEI.setLTCGWhereSTTPaid(indianCurrencyHelper.bigIntegerRoundStr(capitalAssetDetail.getCapitalGainTaxLT()));
+		if(otherSourcesDocument != null){
+			scheduleEI.setInterestInc(indianCurrencyHelper.bigIntegerRound(otherSourcesDocument.getBank_detail_fdr()));
+			scheduleEI.setDividendInc(indianCurrencyHelper.bigIntegerRound(otherSourcesDocument.getDividends()));
+			scheduleEI.setNetAgriIncOrOthrIncRule7(indianCurrencyHelper.bigIntegerRound(otherSourcesDocument.getAgriculture_income()));
+			scheduleEI.setOthers(indianCurrencyHelper.bigIntegerRound(otherSourcesDocument.getOtherincome()));
+		}else{
+			scheduleEI.setInterestInc(new BigInteger("0"));
+			scheduleEI.setDividendInc(new BigInteger("0"));
+			scheduleEI.setNetAgriIncOrOthrIncRule7(new BigInteger("0"));
+			scheduleEI.setOthers(new BigInteger("0"));
 		}
-		 */
-		
-		scheduleEI.setNetAgriIncOrOthrIncRule7(indianCurrencyHelper.bigIntegerRound(otherSourcesDocument.getAgriculture_income()));
-		scheduleEI.setOthers(indianCurrencyHelper.bigIntegerRound(otherSourcesDocument.getOtherincome()));
-		scheduleEI.setTotalExemptInc(indianCurrencyHelper.bigIntegerRound(otherSourcesDocument.getTotal_taxfree_income()));
+		if(capitalAssetDocument != null){
+
+			/* commented for now.. waiting to complete capital gain module
+			 * List<CapitalAssetDetail> capitalAssetDetails = capitalAssetDocument.getCapitalAssetDetailList();
+			for(CapitalAssetDetail capitalAssetDetail:capitalAssetDetails){
+				System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"+indianCurrencyHelper.bigIntegerRoundStr(capitalAssetDetail.getCapitalGainTaxLT()));
+				scheduleEI.setLTCGWhereSTTPaid(indianCurrencyHelper.bigIntegerRoundStr(capitalAssetDetail.getCapitalGainTaxLT()));
+			}
+			 */
+		}else{
+			scheduleEI.setLTCGWhereSTTPaid(indianCurrencyHelper.bigIntegerRound(0d));
+		}
+		scheduleEI.setTotalExemptInc(scheduleEI.getInterestInc().add(scheduleEI.getDividendInc()).add(scheduleEI.getNetAgriIncOrOthrIncRule7())
+				.add(scheduleEI.getOthers()).add(scheduleEI.getLTCGWhereSTTPaid()));
+		if(scheduleEI.getTotalExemptInc() != null && scheduleEI.getTotalExemptInc().longValue() != 0){
 			return scheduleEI;
+		}else
+			return null;
+
 	}
 }
