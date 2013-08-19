53bec3b1588830530df0f7408f895127f4ae6398
Priyank Gupta <pgupta@mootly.com>
2013-08-19 20:07:57 +0000

    ITR2 - Complete of schedule Si rather than testing with Capital gain

--- site/src/main/resources/scripts/ScheduleSI.js
+++ site/src/main/resources/scripts/ScheduleSI.js
@@ -12,9 +12,10 @@ var minCharTaxIn = 0;var calTaxOnIncome = 0;var cal= 0; var taxOnIncome =0; var 
 
 var ltCapGainWIndex = 0;var ltUnlisSec = 0;var stSecA = 0;var stLoss = 0;var stBalnc = 0; var ltCapGainWtIndex = 0;var ltBalnc = 0;var ltded = 0;var totalIncome = 0;
 
-var condition_1=false;var condition_2 = false;var condition_3 = false;var condition_4 = false; var ltUnlisSecWIndex = 0;
+var condition_1 = 0;var condition_2 = 0; var ltUnlisSecWIndex = 0;var remainSlab = 0;
 
 if(scheduleSiDocument!=null){
+	//Schedule SI document Calculation for Total of all Child.
 	if(scheduleSiDocument.getScheduleSiDetailList() != null &&  scheduleSiDocument.getScheduleSiDetailList().size() > 0 ){         		
 		for (var i=0;i<scheduleSiDocument.getScheduleSiDetailList().size();i++) {
 			if(!scheduleSiDocument.getScheduleSiDetailList().get(i).isMarkedForDeletion()) {
@@ -29,9 +30,11 @@ if(scheduleSiDocument!=null){
 
 if(userAmount!=null && spRate!=null){
 	SISectionAmount = userAmount;
+	minCharIncome = SISectionAmount;
 }
 
 if(CapDoc!="CapDoc"){
+	//if you have capital gain document 
 	if(CapDoc.getCapitalAssetDetailList() != null &&  CapDoc.getCapitalAssetDetailList().size() > 0 ){ 
 		for(var i=0;i< CapDoc.getCapitalAssetDetailList().size();i++) {
 			if(java.lang.String.valueOf(CapDoc.getCapitalAssetDetailList().get(i).getSstCharge()) == 'Y'){
@@ -52,39 +55,39 @@ if(CapDoc!="CapDoc"){
 			}
 		}
 	}
-	stGainSTTpaid = stBalnc + stSecA + stLoss;
+	//calculate the values on capital gain according to Schedule SI Sections.
+	stGainSTTpaid = stBalnc + stSecA + stLoss; 
 	ltCapGainWtIndex = (ltBalnc > ltded) ? (ltBalnc - ltded) : 0;	
 	ltGain = (ltCapGainWIndex > ltUnlisSecWIndex) ? (ltCapGainWIndex - ltUnlisSecWIndex) : 0;
-
-	condition_1 = ((stGainSTTpaid > ltCapGainWtIndex) && (stGainSTTpaid > ltUnlisSec) && (stGainSTTpaid >ltGain )) ? true : false; 
-	condition_2 = ((ltCapGainWtIndex > stGainSTTpaid) && (ltCapGainWtIndex > ltUnlisSec) && (ltCapGainWtIndex >ltGain )) ? true : false; 
-	condition_3 = ((ltUnlisSec > ltCapGainWtIndex) && (ltUnlisSec > stGainSTTpaid) && (ltUnlisSec >ltGain )) ? true : false; 
-	condition_4 = ((ltGain > ltCapGainWtIndex) && (ltGain > stGainSTTpaid) && (ltGain >ltUnlisSec )) ? true : false;
+	
+    //adjustment of capital gain if user don't have any tax at source of income.
+	remainSlab = (grossTotal < slabValue) ? (slabValue - grossTotal) : 0; //find remaining amount that can be adjust to capital gain
+	
+	condition_1 = (remainSlab > stGainSTTpaid) ? (remainSlab - stGainSTTpaid) :0; 
+	condition_2 = (condition_1 > ltCapGainWtIndex) ? (condition_1 - ltCapGainWtIndex) :0; 
 
 	if(xmlCode == '1A'){
-		SISectionAmount = stGainSTTpaid; //capDoc		
+		SISectionAmount = stGainSTTpaid; //capDoc	
+		minCharIncome = (remainSlab > SISectionAmount) ? 0 : (SISectionAmount - remainSlab);
 	}
 	if(xmlCode == '22'){
-		SISectionAmount = ltCapGainWtIndex; //capDoc		
+		SISectionAmount = ltCapGainWtIndex; //capDoc
+		minCharIncome = (condition_1 > SISectionAmount) ? 0 : (SISectionAmount - condition_1);
 	}
 	if(xmlCode == '21ciii'){
 		SISectionAmount = ltUnlisSec; //capDoc
+		minCharIncome = SISectionAmount; //this section is only for NRI 
 	}
 	if(xmlCode == '21'){
 		SISectionAmount = ltGain; //capDoc
+		minCharIncome = (condition_2 > SISectionAmount) ? 0 : (SISectionAmount - condition_2);
 	}
-}
-if(condition_1 ||condition_2 ||condition_3 ||condition_4){
-	if(grossTotal < slabValue){
-		minCharIncome = (SISectionAmount > slabValue) ? (SISectionAmount - slabValue) : 0;	
-	}
-}else{
-	minCharIncome = SISectionAmount;
 }
 
 if(spRate!=null){
 	cal = (minCharIncome * spRate)/100;
 }
+
 taxOnIncome = java.lang.Double.parseDouble(java.lang.String.valueOf(indianCurrencyHelper.roundOff(cal)));
 
 out_taxOnIncome = taxOnIncome;
