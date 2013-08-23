a705af3bfef36849dea461d0b873e544d61022a8
Dhananjay <dpanwar@mootly.com>
2013-08-23 12:40:24 +0000

    added for capital gain calculation used in XML

---
+++ site/src/main/resources/scripts/scheduleCG.js
@@ -0,0 +1,18 @@
+
+//Claculation for Short Tern Capital Gain
+
+out_total_dedn = aquis + improv + expend;
+out_balance = fullConsi - out_total_dedn;
+out_balCG = out_balance + loss - dedn;
+out_totCG = out_balCG + nri111A + nri111AN + deemedAmt + nri48A + unlstsec;
+out_fullConsi = fullConsi;
+out_aquis = aquis;
+out_improv = improv;
+out_expend = expend;
+out_loss = loss;
+out_dedn = dedn;
+out_nri111A = nri111A;
+out_nri111AN = nri111AN;
+out_deemedAmt = deemedAmt;
+out_nri48A = nri48A;
+out_unlstsec = unlstsec;
