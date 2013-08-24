cc386e7ab8a64f8f0193d52ee8f83a4edb2bd0ab
Dhananjay <dpanwar@mootly.com>
2013-08-24 06:18:55 +0000

    changes made for scheduleCG.js

--- site/src/main/resources/scripts/scheduleCG.js
+++ site/src/main/resources/scripts/scheduleCG.js
@@ -2,8 +2,8 @@
 //Claculation for Short Tern Capital Gain
 
 out_total_dedn = aquis + improv + expend;
-out_balance = fullConsi - out_total_dedn;
-out_balCG = out_balance + loss - dedn;
+//out_balance = fullConsi - out_total_dedn;
+out_balCG = balance + loss - dedn;
 out_totCG = out_balCG + nri111A + nri111AN + deemedAmt + nri48A + unlstsec;
 out_fullConsi = fullConsi;
 out_aquis = aquis;
@@ -16,3 +16,4 @@ out_nri111AN = nri111AN;
 out_deemedAmt = deemedAmt;
 out_nri48A = nri48A;
 out_unlstsec = unlstsec;
+out_balance = balance;
