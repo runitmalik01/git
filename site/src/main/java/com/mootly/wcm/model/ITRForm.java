e50275231bedf3717232af56c3437a6b64551e52
Dhananjay <dpanwar@mootly.com>
2013-08-24 11:43:56 +0000

    now ITR 2 is available changes made for ITRForm

--- site/src/main/java/com/mootly/wcm/model/ITRForm.java
+++ site/src/main/java/com/mootly/wcm/model/ITRForm.java
@@ -1,22 +1,23 @@
 package com.mootly.wcm.model;
 
 public enum ITRForm {
-	ITR1("ITR-1",new ITRServiceDelivery[] {ITRServiceDelivery.DIY,ITRServiceDelivery.Assisted}),
-	ITR2("ITR-2",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted}),
-	ITR3("ITR-3",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted}),
-	ITR4("ITR-4",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted}),
-	ITR4S("ITR-4S",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted}),
-	ITR5("ITR-5",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted}),
-	ITR6("ITR-6",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted}),
-	ITR7("ITR-7",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted}),
-	ITR8("ITR-8",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted}),
+	ITR1("ITR-1",new ITRServiceDelivery[] {ITRServiceDelivery.DIY,ITRServiceDelivery.Assisted},1),
+	ITR2("ITR-2",new ITRServiceDelivery[] {ITRServiceDelivery.DIY,ITRServiceDelivery.Assisted},2),
+	ITR3("ITR-3",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},3),
+	ITR4("ITR-4",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},4),
+	ITR4S("ITR-4S",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},5),
+	ITR5("ITR-5",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},6),
+	ITR6("ITR-6",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},7),
+	ITR7("ITR-7",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},8),
+	ITR8("ITR-8",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},9),
 	UNKNOWN;
-	
+
 	String whoCanAndWhoCannot;
 	String displayName;
 	Boolean hasDIY = Boolean.FALSE;
 	ITRServiceDelivery[] serviceDeliveryOptions;
-	
+	int priority;
+
 	public String getDisplayName() {
 		return displayName;
 	}
@@ -27,17 +28,17 @@ public enum ITRForm {
 
 	ITRForm() {
 	}
-	
+
 	ITRForm(String displayName) {
 		this.displayName = displayName;
 		this.hasDIY = false;
 	}
-	
+
 	ITRForm(String displayName,Boolean hasDIY) {
 		this.displayName = displayName;
 		this.hasDIY = true;
 	}
-	
+
 	ITRForm(String displayName,ITRServiceDelivery[] serviceDeliveryOptions) {
 		this.displayName = displayName;
 		this.serviceDeliveryOptions = serviceDeliveryOptions;
@@ -50,15 +51,28 @@ public enum ITRForm {
 			}
 		}
 	}
-	
+	ITRForm(String displayName,ITRServiceDelivery[] serviceDeliveryOptions,int priority) {
+		this.displayName = displayName;
+		this.serviceDeliveryOptions = serviceDeliveryOptions;
+		this.hasDIY = false;
+		this.priority = priority;
+		if (serviceDeliveryOptions != null && serviceDeliveryOptions.length >0) {
+			for (ITRServiceDelivery serviceDelivery:serviceDeliveryOptions) {
+				if (serviceDelivery == ITRServiceDelivery.DIY) {
+					this.hasDIY = true;
+				}
+			}
+		}
+	}
+
 	public Boolean getHasDIY() {
 		return hasDIY;
 	}
-	
+
 	public ITRServiceDelivery[] getServiceDeliveryOptions() {
 		return this.serviceDeliveryOptions;
 	}
-	
+
 	public static ITRForm getEnumByDisplayName(String displayName) {
 		if (displayName == null) return UNKNOWN;
 		for (ITRForm aStatus:ITRForm.values()) {
@@ -68,5 +82,9 @@ public enum ITRForm {
 		}
 		return UNKNOWN;
 	}
-	
+
+	public int getPriority() {
+		return this.priority;
+	}
+
 }
