548ed1dd3d8609ebe45d9ae989f56b6280044e7b
Dhananjay <dpanwar@mootly.com>
2013-09-05 08:19:26 +0000

    added TCSSchedule.java for ITR4S

---
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/TCSSchedule.java
@@ -0,0 +1,51 @@
+package com.mootly.wcm.model.schedules.y2012_2013;
+
+import java.util.List;
+
+import in.gov.incometaxindiaefiling.y2012_2013.EmployerOrDeductorOrCollectDetl;
+import in.gov.incometaxindiaefiling.y2012_2013.ITR;
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleTCS;
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleTCS.TCS;
+import in.gov.incometaxindiaefiling.y2012_2013.TaxPayment;
+
+import com.mootly.wcm.beans.TcsDocument;
+import com.mootly.wcm.beans.compound.AdvanceTaxDetail;
+import com.mootly.wcm.beans.compound.TcsDetail;
+import com.mootly.wcm.services.IndianCurrencyHelper;
+
+public class TCSSchedule {
+
+	TcsDocument tcsDocument = null;
+
+	public TCSSchedule(TcsDocument tcsDocument){
+		this.tcsDocument = tcsDocument;
+	}
+
+	public ScheduleTCS getScheduleTCS(ITR itr){
+
+		ScheduleTCS scheduleTCS = new ScheduleTCS();
+		IndianCurrencyHelper indianCurrencyHelper = new IndianCurrencyHelper();
+
+		boolean hasAValidTCS = false;
+		if(tcsDocument != null){
+			List<TcsDetail> tcsDetails = tcsDocument.getTcsDetailList();
+			if (tcsDetails != null && tcsDetails.size() > 0 ){
+				for (TcsDetail tcsDetail:tcsDetails) {
+					TCS tCS = new TCS();
+					EmployerOrDeductorOrCollectDetl employerOrDeductorOrCollectDetl = new EmployerOrDeductorOrCollectDetl();
+					employerOrDeductorOrCollectDetl.setEmployerOrDeductorOrCollecterName(tcsDetail.getName());
+					employerOrDeductorOrCollectDetl.setTAN(tcsDetail.getTan());
+					tCS.setEmployerOrDeductorOrCollectDetl(employerOrDeductorOrCollectDetl);
+					tCS.setAmtTCSClaimedThisYear(indianCurrencyHelper.bigIntegerRound(tcsDetail.getTaxCredited()));
+					tCS.setTotalTCS(indianCurrencyHelper.bigIntegerRound(tcsDetail.getTotaltax()));
+					scheduleTCS.getTCS().add(tCS);
+					if(!hasAValidTCS) hasAValidTCS = true;
+				}
+			}
+		}
+		if(hasAValidTCS){
+			return scheduleTCS;
+		}else
+			return null;
+	}
+}
