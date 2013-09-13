5bc039bb8e6a5bd70546661793d080800c380779
Dhananjay <dpanwar@mootly.com>
2013-09-13 11:57:11 +0000

    added Form_ITR4.java for ITR4

---
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/Form_ITR4.java
@@ -0,0 +1,18 @@
+package com.mootly.wcm.model.schedules.y2012_2013;
+
+import in.gov.incometaxindiaefiling.y2012_2013.FormITR4;
+import in.gov.incometaxindiaefiling.y2012_2013.ITR;
+
+public class Form_ITR4 {
+
+	public FormITR4 getFormITR4(ITR itr){
+
+		FormITR4 formITR4 = new FormITR4();
+		formITR4.setFormName("ITR-4");
+		formITR4.setDescription("For indls and HUFs having income from a proprietory business or profession");
+		formITR4.setAssessmentYear("2013");
+		formITR4.setSchemaVer("Ver1.0");
+		formITR4.setFormVer("Ver1.0");
+		return formITR4;
+	}
+}
