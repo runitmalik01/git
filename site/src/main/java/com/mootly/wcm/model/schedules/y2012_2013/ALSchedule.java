142363d2b55a210988e3fd4dece048757c07b9c2
Dhananjay <dpanwar@mootly.com>
2013-09-14 11:23:37 +0000

    added ScheduleAL for ITR4 XML

---
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ALSchedule.java
@@ -0,0 +1,58 @@
+package com.mootly.wcm.model.schedules.y2012_2013;
+
+import java.lang.reflect.Field;
+import java.math.BigInteger;
+
+import org.apache.james.mime4j.field.Fields;
+import org.springframework.beans.DirectFieldAccessor;
+
+import in.gov.incometaxindiaefiling.y2012_2013.ITR;
+import in.gov.incometaxindiaefiling.y2012_2013.MovableAsset;
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleAL;
+
+import com.mootly.wcm.beans.AssetAndLiabilityDocument;
+import com.mootly.wcm.services.IndianCurrencyHelper;
+
+public class ALSchedule {
+
+	AssetAndLiabilityDocument assetAndLiabilityDocument = null;
+
+	public ALSchedule(AssetAndLiabilityDocument assetAndLiabilityDocument){
+		this.assetAndLiabilityDocument = assetAndLiabilityDocument;
+	}
+
+	public ScheduleAL getScheduleAL(ITR itr){
+
+		IndianCurrencyHelper indianCurrencyHelper = new IndianCurrencyHelper();
+		ScheduleAL scheduleAL = new ScheduleAL();
+
+		if(assetAndLiabilityDocument == null){
+			AssetAndLiabilityDocument assetAndLiabilityDocumentDummy = new AssetAndLiabilityDocument();
+			DirectFieldAccessor directFieldAccessor = new DirectFieldAccessor(assetAndLiabilityDocumentDummy);
+			Field[] fields =  AssetAndLiabilityDocument.class.getDeclaredFields();
+			for(Field field : fields){
+				if(field.getType().getSimpleName().equals(Double.class.getSimpleName())){
+					directFieldAccessor.setPropertyValue(field.getName(), 0d);
+				}
+			}
+			this.assetAndLiabilityDocument = assetAndLiabilityDocumentDummy;
+		}
+
+		scheduleAL.setImmovableAssetLand(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getLand()));
+		scheduleAL.setImmovableAssetBuilding(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getBuilding()));
+		MovableAsset movableAsset = new MovableAsset();
+		movableAsset.setDepositsInBank(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getDeposit_Bank()));
+		movableAsset.setSharesAndSecurities(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getShares()));
+		movableAsset.setInsurancePolicies(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getInsurance()));
+		movableAsset.setLoansAndAdvancesGiven(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getLoans_Adv()));
+		movableAsset.setCashInHand(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getCash()));
+		movableAsset.setJewelleryBullionEtc(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getJewellery()));
+		movableAsset.setArchCollDrawPaintSulpArt(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getDrawing()));
+		movableAsset.setVehiclYachtsBoatsAircrafts(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getVehicles()));
+		movableAsset.setTotalImmovablMovablAssets(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getTotal()));
+		scheduleAL.setMovableAsset(movableAsset);
+		scheduleAL.setLiabilityInRelatAssets(indianCurrencyHelper.bigIntegerRound(assetAndLiabilityDocument.getLiability()));
+
+		return scheduleAL;
+	}
+}
