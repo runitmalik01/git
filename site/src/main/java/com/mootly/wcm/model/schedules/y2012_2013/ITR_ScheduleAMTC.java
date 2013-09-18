563e3a35e33e9edb9fb3314fef56a5f98bc546af
Priyank Gupta <pgupta@mootly.com>
2013-09-18 05:54:49 +0000

    correcting schedule AMTC (return ScheduleAMTC object)

--- site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ITR_ScheduleAMTC.java
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ITR_ScheduleAMTC.java
@@ -1,16 +1,40 @@
 package com.mootly.wcm.model.schedules.y2012_2013;
 
 import in.gov.incometaxindiaefiling.y2012_2013.ITR;
+import in.gov.incometaxindiaefiling.y2012_2013.ITRScheduleAMTC;
+import in.gov.incometaxindiaefiling.y2012_2013.PartBTTI;
 import in.gov.incometaxindiaefiling.y2012_2013.ScheduleAMTC;
 
 import java.lang.reflect.Field;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.springframework.beans.DirectFieldAccessor;
 
+import com.mootly.wcm.beans.AdvanceTaxDocument;
 import com.mootly.wcm.beans.BalanceSheetDocument;
+import com.mootly.wcm.beans.CapitalAssetDocument;
+import com.mootly.wcm.beans.DeductionDocument;
+import com.mootly.wcm.beans.DetailOfTrustDocument;
+import com.mootly.wcm.beans.FinancialInterestDocument;
+import com.mootly.wcm.beans.ForeignBankAccountDocument;
+import com.mootly.wcm.beans.FormSixteenDocument;
+import com.mootly.wcm.beans.HouseProperty;
+import com.mootly.wcm.beans.ImmovablePropertyDocument;
+import com.mootly.wcm.beans.MemberPersonalInformation;
+import com.mootly.wcm.beans.NatureInvestmentDocument;
+import com.mootly.wcm.beans.OtherSourcesDocument;
+import com.mootly.wcm.beans.SalaryIncomeDocument;
 import com.mootly.wcm.beans.ScheduleAMTCDocument;
+import com.mootly.wcm.beans.ScheduleSIDocument;
+import com.mootly.wcm.beans.SelfAssesmetTaxDocument;
+import com.mootly.wcm.beans.SigningAuthorityAccountsDocument;
+import com.mootly.wcm.beans.TaxReliefDocument;
+import com.mootly.wcm.beans.TcsDocument;
+import com.mootly.wcm.beans.TdsFromothersDocument;
 import com.mootly.wcm.model.FinancialYear;
 import com.mootly.wcm.services.IndianCurrencyHelper;
 
@@ -23,8 +47,38 @@ public class ITR_ScheduleAMTC {
 		this.scheduleAMTCDocument = scheduleAMTCDocument;
 	}
 	
-	public ScheduleAMTC getScheduleAMTC(ITR itr, FinancialYear financialYear, Map<String, HippoBean> inputBeans){
+	public ITRScheduleAMTC getScheduleAMTC(ITR itr, FinancialYear financialYear, Map<String, HippoBean> inputBeans){
+		
+		MemberPersonalInformation memberPersonalInformation = (MemberPersonalInformation) inputBeans.get(MemberPersonalInformation.class.getSimpleName().toLowerCase());
+		SalaryIncomeDocument salaryIncomeDocument = (SalaryIncomeDocument) inputBeans.get(SalaryIncomeDocument.class.getSimpleName().toLowerCase());
+		HouseProperty houseProperty = (HouseProperty) inputBeans.get(HouseProperty.class.getSimpleName().toLowerCase());
+		OtherSourcesDocument otherSourcesDocument = (OtherSourcesDocument) inputBeans.get(OtherSourcesDocument.class.getSimpleName().toLowerCase());
+		AdvanceTaxDocument advanceTaxDocument = (AdvanceTaxDocument) inputBeans.get(AdvanceTaxDocument.class.getSimpleName().toLowerCase());
+		TdsFromothersDocument tdsFromothersDocument = (TdsFromothersDocument) inputBeans.get(TdsFromothersDocument.class.getSimpleName().toLowerCase());
+		SelfAssesmetTaxDocument selfAssesmetTaxDocument = (SelfAssesmetTaxDocument) inputBeans.get(SelfAssesmetTaxDocument.class.getSimpleName().toLowerCase());
+		DeductionDocument deductionDocument = (DeductionDocument) inputBeans.get(DeductionDocument.class.getSimpleName().toLowerCase());
+		FormSixteenDocument formSixteenDocument = (FormSixteenDocument) inputBeans.get(FormSixteenDocument.class.getSimpleName().toLowerCase());
+		TaxReliefDocument taxReliefDocument = (TaxReliefDocument) inputBeans.get(TaxReliefDocument.class.getSimpleName().toLowerCase());
+		ImmovablePropertyDocument immovablePropertyDocument = (ImmovablePropertyDocument) inputBeans.get(ImmovablePropertyDocument.class.getSimpleName().toLowerCase());
+		NatureInvestmentDocument natureInvestmentDocument = (NatureInvestmentDocument) inputBeans.get(NatureInvestmentDocument.class.getSimpleName().toLowerCase());
+		SigningAuthorityAccountsDocument signingAuthorityAccountsDocument = (SigningAuthorityAccountsDocument) inputBeans.get(SigningAuthorityAccountsDocument.class.getSimpleName().toLowerCase());
+		DetailOfTrustDocument detailOfTrustDocument = (DetailOfTrustDocument) inputBeans.get(DetailOfTrustDocument.class.getSimpleName().toLowerCase());
+		ForeignBankAccountDocument foreignBankAccountDocument = (ForeignBankAccountDocument) inputBeans.get(ForeignBankAccountDocument.class.getSimpleName().toLowerCase());
+		FinancialInterestDocument financialInterestDocument = (FinancialInterestDocument) inputBeans.get(FinancialInterestDocument.class.getSimpleName().toLowerCase());
+		CapitalAssetDocument capitalAssetDocument = (CapitalAssetDocument) inputBeans.get(CapitalAssetDocument.class.getSimpleName().toLowerCase());
+		ScheduleSIDocument scheduleSIDocument = (ScheduleSIDocument) inputBeans.get(ScheduleSIDocument.class.getSimpleName().toLowerCase());
+		TcsDocument tcsDocument = (TcsDocument) inputBeans.get(TcsDocument.class.getSimpleName().toLowerCase());
+		
+		PartB_TTI partB_TTI = new PartB_TTI(formSixteenDocument, salaryIncomeDocument, houseProperty,
+				otherSourcesDocument, deductionDocument, memberPersonalInformation, taxReliefDocument, advanceTaxDocument,
+				selfAssesmetTaxDocument, tdsFromothersDocument, scheduleSIDocument, capitalAssetDocument, immovablePropertyDocument,
+				natureInvestmentDocument,signingAuthorityAccountsDocument,detailOfTrustDocument,foreignBankAccountDocument,financialInterestDocument,
+				tcsDocument);
+		PartBTTI partBTTI = partB_TTI.getPartBTTI(itr, financialYear, inputBeans);
+		
 		IndianCurrencyHelper currencyHelper = new IndianCurrencyHelper();
+		List<ScheduleAMTC> scheduleAMTCList = new ArrayList<ScheduleAMTC>();
+		ITRScheduleAMTC itrScheduleAMTC = new ITRScheduleAMTC();
 		ScheduleAMTC scheduleAMTC = new ScheduleAMTC();
 		scheduleAMTC.setAssYr(financialYear.getDisplayAssessmentYear());
 		if(scheduleAMTCDocument == null){
@@ -44,7 +98,19 @@ public class ITR_ScheduleAMTC {
 			scheduleAMTC.setAmtCreditSetOfEy(currencyHelper.bigIntegerRound(scheduleAMTCDocument.getAmtCreditSetOff()));
 			scheduleAMTC.setAmtCreditBalBroughtFwd(currencyHelper.bigIntegerRound(scheduleAMTCDocument.getAmtCreditBrghtFwrd()));
 		}
+		scheduleAMTCList.add(scheduleAMTC);
+		itrScheduleAMTC.getScheduleAMTC().addAll(scheduleAMTCList);
+		itrScheduleAMTC.setTaxSection115JC(partBTTI.getComputationOfTaxLiability().getTaxPayableOnDeemedTI().getTotalTax());
+		itrScheduleAMTC.setTaxOthProvisions(partBTTI.getComputationOfTaxLiability().getGrossTaxLiability());
+		itrScheduleAMTC.setAmtTaxCreditAvailable(itrScheduleAMTC.getTaxOthProvisions().subtract(itrScheduleAMTC.getTaxSection115JC()).doubleValue() > 0 ? itrScheduleAMTC.getTaxOthProvisions().subtract(itrScheduleAMTC.getTaxSection115JC()) : new BigInteger("0"));
+		itrScheduleAMTC.setTotAMTGross(currencyHelper.bigIntegerRound(scheduleAMTCDocument.getAmtCreditGross()));
+		itrScheduleAMTC.setTotSetOffEys(currencyHelper.bigIntegerRound(scheduleAMTCDocument.getAmtCreditSetOff()));
+		itrScheduleAMTC.setTotBalBF(currencyHelper.bigIntegerRound(scheduleAMTCDocument.getAmtCreditBrghtFwrd()));
+		itrScheduleAMTC.setTotAmtCreditUtilisedCY(currencyHelper.bigIntegerRound(scheduleAMTCDocument.getAmtCreditUnlisted()));
+		itrScheduleAMTC.setTotBalAMTCreditCF(currencyHelper.bigIntegerRound(scheduleAMTCDocument.getAmtCreditCarriedFwrd()));
+		itrScheduleAMTC.setTaxSection115JD(currencyHelper.bigIntegerRound(scheduleAMTCDocument.getAmtCreditUnlisted()));
+		itrScheduleAMTC.setAmtLiabilityAvailable(currencyHelper.bigIntegerRound(scheduleAMTCDocument.getAmtCreditCarriedFwrd()));
 		
-		return scheduleAMTC;
+		return itrScheduleAMTC;
 	} 
 }
