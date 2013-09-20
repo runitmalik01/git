2a225fc912b0315d51738d7b1e31e32f67b1ed87
Dhananjay <dpanwar@mootly.com>
2013-09-20 10:07:24 +0000

    changes made for ITRForm.java

--- site/src/main/java/com/mootly/wcm/model/ITRForm.java
+++ site/src/main/java/com/mootly/wcm/model/ITRForm.java
@@ -4,7 +4,7 @@ public enum ITRForm {
 	ITR1("ITR-1",new ITRServiceDelivery[] {ITRServiceDelivery.DIY,ITRServiceDelivery.Assisted},1),
 	ITR2("ITR-2",new ITRServiceDelivery[] {ITRServiceDelivery.DIY,ITRServiceDelivery.Assisted},2),
 	ITR3("ITR-3",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},3),
-	ITR4("ITR-4",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},4),
+	ITR4("ITR-4",new ITRServiceDelivery[] {ITRServiceDelivery.DIY,ITRServiceDelivery.Assisted},4),
 	ITR4S("ITR-4S",new ITRServiceDelivery[] {ITRServiceDelivery.DIY, ITRServiceDelivery.Assisted},5),
 	ITR5("ITR-5",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},6),
 	ITR6("ITR-6",new ITRServiceDelivery[] {ITRServiceDelivery.Assisted},7),
