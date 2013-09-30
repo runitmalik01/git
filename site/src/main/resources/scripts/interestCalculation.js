7de0396f91e7027693e2b8440567f1af7798f380
Dhananjay <dpanwar@mootly.com>
2013-09-30 08:32:33 +0000

    changes made for interestCalculation.js

--- site/src/main/resources/scripts/interestCalculation.js
+++ site/src/main/resources/scripts/interestCalculation.js
@@ -1,6 +1,7 @@
 
 //var aysfall = $('#aytaxd').val()-$('#aytaxp').val();
 var aysfall = aytaxd-aytaxp;
+/*
 if(aysfall<0){
 	aysfall = 0;
 }
@@ -16,15 +17,21 @@ if(ddate==7 && aytaxmp>7){  // Due date is 31 july after that interest will be c
 		else {
 			var dateA = 0;
 		}
+
 var dateB = aytaxmp-3;
 if( dateB<0){
 	dateB = 0 ;
 }
+*/
+var dateA = dueDateFor234A;
 out_intA = Math.round(aysfall * dateA/100);
+/*
 if(aytaxd>=10000){    // Added on 31/07/2013 for interest Section234B
 	out_intB = Math.round(aysfall * dateB/100);
 }else
 	out_intB = 0;
+	*/
+out_intB = resultIntB;
 
 var atdq2 = Math.round(aytaxd*30/100);
 var atsfq2 = atdq2-atlq2-atpq2;
