cdcac947350ee1634d29767ddb601a5b230a2dbb
Dhananjay <dpanwar@mootly.com>
2013-09-30 08:32:55 +0000

    changes made for lossesCalculation.js

--- site/src/main/resources/scripts/lossesCalculation.js
+++ site/src/main/resources/scripts/lossesCalculation.js
@@ -18,6 +18,22 @@ out_balancedHouseIncomeAftRH=0;
 out_totalHPLossSetoff = 0;
 out_totalOILossSetoff = 0;
 //out_balancedSTCLoss = 0;
+//added for ITR4
+out_balancedBusinessIncomeAftHI = 0;
+out_totalBILossSetoff = 0;
+out_balancedBusinessIncomeAftSTC = 0;
+out_balancedBusinessIncomeAftLTC = 0;
+out_balancedBusinessIncomeAftOI = 0;
+out_balancedBusinessIncomeAftHR = 0;
+out_balancedHouseIncomeAftBI = 0;
+out_balancedOtherIncomeAftBI = 0;
+out_adjustedBusinessIncome = 0;
+out_balancedHouseIncomeAftSI = 0;
+out_balancedOtherIncomeAftSI = 0;
+out_adjustedSpeculativeIncome = 0;
+out_balancedHouseIncomeAftSPI = 0;
+out_balancedOtherIncomeAftSPI = 0;
+out_adjustedSpecifiedIncome = 0;
 
 /*
  * Calculation for CYLA(Adjustment of losses) begins
@@ -53,6 +69,21 @@ if(salaryIncome>0){
 }
 
 if(houseIncome>0){
+	//added for ITR4 to set Business Income loss with House Income
+	if(businessIncome<0){
+		houseIncome = houseIncome - (businessIncome*(-1));
+		if(houseIncome>0){
+			out_balancedBusinessIncomeAftHI = businessIncome*(-1);
+			businessIncome = 0;
+		}
+		if(houseIncome<=0){
+			out_balancedBusinessIncomeAftHI = houseIncome + (businessIncome*(-1));
+			businessIncome = houseIncome;
+			houseIncome = 0;
+		}
+		out_totalBILossSetoff = out_totalBILossSetoff + out_balancedBusinessIncomeAftHI;
+	}
+	//end
 	if(otherIncome<0){
 		houseIncome = houseIncome - (otherIncome*(-1));
 		if(houseIncome>0){
@@ -69,6 +100,100 @@ if(houseIncome>0){
 	out_adjustedHouseIncome = houseIncome;
 }
 
+//added for ITR4 to adjust losses with Business Income
+if(businessIncome>0){
+	if(houseIncome<0){
+		businessIncome = businessIncome - (houseIncome*(-1));
+		if(businessIncome>0){
+			out_balancedHouseIncomeAftBI = houseIncome*(-1);
+			houseIncome = 0;
+		}
+		if(businessIncome<=0){
+			out_balancedHouseIncomeAftBI =  businessIncome + (houseIncome*(-1));
+			houseIncome = businessIncome;
+			businessIncome = 0;
+		}
+		out_totalHPLossSetoff = out_totalHPLossSetoff + out_balancedHouseIncomeAftBI;
+	}
+	if(otherIncome<0){
+		businessIncome = businessIncome - (otherIncome*(-1));
+		if(businessIncome>0){
+			out_balancedOtherIncomeAftBI = otherIncome*(-1);
+			otherIncome = 0;
+		}
+		if(businessIncome<=0){
+			out_balancedOtherIncomeAftBI = businessIncome + (otherIncome*(-1));
+			otherIncome = businessIncome;
+			businessIncome = 0;
+		}
+		out_totalOILossSetoff = out_totalOILossSetoff + out_balancedOtherIncomeAftBI;
+	}
+	out_adjustedBusinessIncome = businessIncome;
+}
+//end
+//added for ITR4 to adjust losses with Speculative Income
+if(speculativeIncome>0){
+	if(houseIncome<0){
+		speculativeIncome = speculativeIncome - (houseIncome*(-1));
+		if(speculativeIncome>0){
+			out_balancedHouseIncomeAftSI = houseIncome*(-1);
+			houseIncome = 0;
+		}
+		if(speculativeIncome<=0){
+			out_balancedHouseIncomeAftSI =  speculativeIncome + (houseIncome*(-1));
+			houseIncome = speculativeIncome;
+			speculativeIncome = 0;
+		}
+		out_totalHPLossSetoff = out_totalHPLossSetoff + out_balancedHouseIncomeAftSI;
+	}
+	if(otherIncome<0){
+		speculativeIncome = speculativeIncome - (otherIncome*(-1));
+		if(speculativeIncome>0){
+			out_balancedOtherIncomeAftSI = otherIncome*(-1);
+			otherIncome = 0;
+		}
+		if(speculativeIncome<=0){
+			out_balancedOtherIncomeAftSI = speculativeIncome + (otherIncome*(-1));
+			otherIncome = speculativeIncome;
+			speculativeIncome = 0;
+		}
+		out_totalOILossSetoff = out_totalOILossSetoff + out_balancedOtherIncomeAftSI;
+	}
+	out_adjustedSpeculativeIncome = speculativeIncome;
+}
+//end
+//added for ITR4 to adjust losses with Specified Income
+if(specifiedIncome>0){
+	if(houseIncome<0){
+		specifiedIncome = specifiedIncome - (houseIncome*(-1));
+		if(specifiedIncome>0){
+			out_balancedHouseIncomeAftSPI = houseIncome*(-1);
+			houseIncome = 0;
+		}
+		if(specifiedIncome<=0){
+			out_balancedHouseIncomeAftSPI =  specifiedIncome + (houseIncome*(-1));
+			houseIncome = specifiedIncome;
+			specifiedIncome = 0;
+		}
+		out_totalHPLossSetoff = out_totalHPLossSetoff + out_balancedHouseIncomeAftSPI;
+	}
+	if(otherIncome<0){
+		specifiedIncome = specifiedIncome - (otherIncome*(-1));
+		if(specifiedIncome>0){
+			out_balancedOtherIncomeAftSPI = otherIncome*(-1);
+			otherIncome = 0;
+		}
+		if(specifiedIncome<=0){
+			out_balancedOtherIncomeAftSPI = specifiedIncome + (otherIncome*(-1));
+			otherIncome = specifiedIncome;
+			specifiedIncome = 0;
+		}
+		out_totalOILossSetoff = out_totalOILossSetoff + out_balancedOtherIncomeAftSPI;
+	}
+	out_adjustedSpecifiedIncome = specifiedIncome;
+}
+//end
+
 if(STCGain>0){
 	if(houseIncome<0){
 		STCGain = STCGain - (houseIncome*(-1));
@@ -83,6 +208,21 @@ if(STCGain>0){
 		}
 		out_totalHPLossSetoff = out_totalHPLossSetoff + out_balancedHouseIncomeAftSTC;
 	}
+	//added for ITR4 to adjust Business Income loss with STCG
+	if(businessIncome<0){
+		STCGain = STCGain - (businessIncome*(-1));
+		if(STCGain>0){
+			out_balancedBusinessIncomeAftSTC = businessIncome*(-1);
+			businessIncome = 0;
+		}
+		if(STCGain<=0){
+			out_balancedBusinessIncomeAftSTC = STCGain + (businessIncome*(-1));
+			businessIncome = STCGain;
+			STCGain = 0;
+		}
+		out_totalBILossSetoff = out_totalBILossSetoff + out_balancedBusinessIncomeAftSTC;
+	}
+	//end
 	if(otherIncome<0){
 		STCGain = STCGain - (otherIncome*(-1));
 		if(STCGain>0){
@@ -113,6 +253,21 @@ if(LTCGain>0){
 		}
 		out_totalHPLossSetoff = out_totalHPLossSetoff + out_balancedHouseIncomeAftLTC;
 	}
+	//added for ITR4 to adjust Business Income loss with LTGC
+	if(businessIncome<0){
+		LTCGain = LTCGain - (businessIncome*(-1));
+		if(LTCGain>0){
+			out_balancedBusinessIncomeAftLTC = businessIncome*(-1);
+			businessIncome = 0;
+		}
+		if(LTCGain<=0){
+			out_balancedBusinessIncomeAftLTC = LTCGain + (businessIncome*(-1));
+			businessIncome = LTCGain;
+			LTCGain = 0;
+		}
+		out_totalBILossSetoff = out_totalBILossSetoff + out_balancedBusinessIncomeAftLTC;
+	}
+	//end
 	if(otherIncome<0){
 		LTCGain = LTCGain - (otherIncome*(-1));
 		if(LTCGain>0){
@@ -156,6 +311,21 @@ if(otherIncome>0){
 		}
 		out_totalHPLossSetoff = out_totalHPLossSetoff + out_balancedHouseIncomeAftOI;
 	}
+	//added for ITR4 to adjust Business Income Loss with Other Income
+	if(businessIncome<0){
+		otherIncome = otherIncome-(businessIncome*(-1));
+		if(otherIncome>0){
+			out_balancedBusinessIncomeAftOI = businessIncome*(-1);
+			businessIncome = 0;
+		}
+		if(otherIncome<=0){
+			out_balancedBusinessIncomeAftOI =  otherIncome + (businessIncome*(-1));
+			businessIncome = otherIncome;
+			otherIncome = 0;
+		}
+		out_totalBILossSetoff = out_totalBILossSetoff + out_balancedBusinessIncomeAftOI;
+	}
+	//end
 	out_adjustedOtherIncome = otherIncome;
 }
 
@@ -173,6 +343,21 @@ if(maintainingRaceHorseIncome>0){
 		}
 		out_totalHPLossSetoff = out_totalHPLossSetoff + out_balancedHouseIncomeAftRH;
 	}
+	//added for ITR4 to adjust Business Income Loss with Maintaining Race Horse Income
+	if(businessIncome<0){
+		maintainingRaceHorseIncome = maintainingRaceHorseIncome - (businessIncome*(-1));
+		if(maintainingRaceHorseIncome>0){
+			out_balancedBusinessIncomeAftHR = businessIncome*(-1);
+			businessIncome = 0;
+		}
+		if(maintainingRaceHorseIncome<=0){
+			out_balancedBusinessIncomeAftHR = maintainingRaceHorseIncome + (businessIncome*(-1));
+			businessIncome = maintainingRaceHorseIncome;
+			maintainingRaceHorseIncome = 0;
+		}
+		out_totalBILossSetoff = out_totalBILossSetoff + out_balancedBusinessIncomeAftHR;
+	}
+	//end
 	if(otherIncome<0){
 		maintainingRaceHorseIncome = maintainingRaceHorseIncome - (otherIncome*(-1));
 		if(maintainingRaceHorseIncome>0){
