2c35a39c39b86dba3415f4843755dc6916d5cdf9
Dhananjay <dpanwar@mootly.com>
2013-09-30 08:27:17 +0000

    changes made for CurrentYearLossesSchedules.java

--- site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/CurrentYearLossesSchedules.java
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/CurrentYearLossesSchedules.java
@@ -11,6 +11,8 @@ import in.gov.incometaxindiaefiling.y2012_2013.ScheduleCYLA.OthSrcExclRaceHorse;
 import in.gov.incometaxindiaefiling.y2012_2013.ScheduleCYLA.OthSrcRaceHorse;
 import in.gov.incometaxindiaefiling.y2012_2013.ScheduleCYLA.STCG;
 import in.gov.incometaxindiaefiling.y2012_2013.ScheduleCYLA.Salary;
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleCYLA.SpecifiedInc;
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleCYLA.SpeculativeInc;
 import in.gov.incometaxindiaefiling.y2012_2013.ScheduleCYLA.TotalCurYr;
 import in.gov.incometaxindiaefiling.y2012_2013.ScheduleCYLA.TotalLossSetOff;
 
@@ -23,6 +25,7 @@ import org.hippoecm.hst.core.component.HstResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mootly.wcm.beans.MemberPersonalInformation;
 import com.mootly.wcm.model.FinancialYear;
 import com.mootly.wcm.services.IndianCurrencyHelper;
 import com.mootly.wcm.utils.XmlCalculation;
@@ -38,6 +41,9 @@ public class CurrentYearLossesSchedules extends XmlCalculation{
 	 */
 
 	public ScheduleCYLA getScheduleCYLA(ITR itr, FinancialYear financialYear,Map<String,HippoBean> inputBeans){
+
+		MemberPersonalInformation memberPersonalInformation = (MemberPersonalInformation) inputBeans.get(MemberPersonalInformation.class.getSimpleName().toLowerCase());
+		String itrSelection =  memberPersonalInformation.getFlexField("flex_string_ITRForm", "");
 
 		IndianCurrencyHelper indianCurrencyHelper = new IndianCurrencyHelper();
 
@@ -59,22 +65,65 @@ public class CurrentYearLossesSchedules extends XmlCalculation{
 			inCCYLA.setIncOfCurYrUnderThatHead(indianCurrencyHelper.longToBigInteger(houseIncomeTotal));
 		}else
 			inCCYLA.setIncOfCurYrUnderThatHead(new BigInteger("0"));
-		inCCYLA.setBusLossSetoff(new BigInteger("0"));
+		inCCYLA.setBusLossSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedBusinessIncomeAftHI").toString())));// for ITR4
 		inCCYLA.setHPlossCurYrSetoff(new BigInteger("0"));
 		inCCYLA.setOthSrcLossNoRaceHorseSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedOtherIncomeAftHI").toString())));
 		inCCYLA.setIncOfCurYrAfterSetOff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("adjustedHouseIncome").toString())));
 		hP.setIncCYLA(inCCYLA);
 		scheduleCYLA.setHP(hP);
 
-		BusProfExclSpecProf busProfExclSpecProf = new BusProfExclSpecProf();
-		IncCYLA iNCCYLA = new IncCYLA();
-		iNCCYLA.setIncOfCurYrUnderThatHead(new BigInteger("0"));
-		iNCCYLA.setBusLossSetoff(new BigInteger("0"));
-		iNCCYLA.setHPlossCurYrSetoff(new BigInteger("0"));
-		iNCCYLA.setOthSrcLossNoRaceHorseSetoff(new BigInteger("0"));
-		iNCCYLA.setIncOfCurYrAfterSetOff(new BigInteger("0"));
-		busProfExclSpecProf.setIncCYLA(iNCCYLA);
-		scheduleCYLA.setBusProfExclSpecProf(busProfExclSpecProf);
+		//for ITR4
+		if(itrSelection.equals("ITR4")){
+			BusProfExclSpecProf busProfExclSpecProf = new BusProfExclSpecProf();
+			IncCYLA iNCCYLA = new IncCYLA();
+			if(businessIncome > 0){
+				iNCCYLA.setIncOfCurYrUnderThatHead(BigInteger.valueOf(businessIncome));
+			}else
+				iNCCYLA.setIncOfCurYrUnderThatHead(new BigInteger("0"));
+			iNCCYLA.setBusLossSetoff(new BigInteger("0"));
+			iNCCYLA.setHPlossCurYrSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedHouseIncomeAftBI").toString())));
+			iNCCYLA.setOthSrcLossNoRaceHorseSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedOtherIncomeAftBI").toString())));
+			iNCCYLA.setIncOfCurYrAfterSetOff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("adjustedBusinessIncome").toString())));
+			busProfExclSpecProf.setIncCYLA(iNCCYLA);
+			scheduleCYLA.setBusProfExclSpecProf(busProfExclSpecProf);
+
+			SpeculativeInc speculativeInc = new SpeculativeInc();
+			IncCYLA iNCCYLAforSI = new IncCYLA();
+			if(speculativeIncome>0){
+				iNCCYLAforSI.setIncOfCurYrUnderThatHead(BigInteger.valueOf(speculativeIncome));
+			}else
+				iNCCYLAforSI.setIncOfCurYrUnderThatHead(new BigInteger("0"));
+			iNCCYLAforSI.setBusLossSetoff(new BigInteger("0"));
+			iNCCYLAforSI.setHPlossCurYrSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedHouseIncomeAftSI").toString())));
+			iNCCYLAforSI.setOthSrcLossNoRaceHorseSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedOtherIncomeAftSI").toString())));
+			iNCCYLAforSI.setIncOfCurYrAfterSetOff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("adjustedSpeculativeIncome").toString())));
+			speculativeInc.setIncCYLA(iNCCYLAforSI);
+			scheduleCYLA.setSpeculativeInc(speculativeInc);
+
+			SpecifiedInc specifiedInc = new SpecifiedInc();
+			IncCYLA iNCCYLAforSPI = new IncCYLA();
+			if(specifiedIncome>0){
+				iNCCYLAforSPI.setIncOfCurYrUnderThatHead(BigInteger.valueOf(specifiedIncome));
+			}else
+				iNCCYLAforSPI.setIncOfCurYrUnderThatHead(new BigInteger("0"));
+			iNCCYLAforSPI.setBusLossSetoff(new BigInteger("0"));
+			iNCCYLAforSPI.setHPlossCurYrSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedHouseIncomeAftSPI").toString())));
+			iNCCYLAforSPI.setOthSrcLossNoRaceHorseSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedOtherIncomeAftSPI").toString())));
+			iNCCYLAforSPI.setIncOfCurYrAfterSetOff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("adjustedSpecifiedIncome").toString())));
+			specifiedInc.setIncCYLA(iNCCYLAforSPI);
+			scheduleCYLA.setSpecifiedInc(specifiedInc);
+		}else{
+			BusProfExclSpecProf busProfExclSpecProf = new BusProfExclSpecProf();
+			IncCYLA iNCCYLA = new IncCYLA();
+			iNCCYLA.setIncOfCurYrUnderThatHead(new BigInteger("0"));
+			iNCCYLA.setBusLossSetoff(new BigInteger("0"));
+			iNCCYLA.setHPlossCurYrSetoff(new BigInteger("0"));
+			iNCCYLA.setOthSrcLossNoRaceHorseSetoff(new BigInteger("0"));
+			iNCCYLA.setIncOfCurYrAfterSetOff(new BigInteger("0"));
+			busProfExclSpecProf.setIncCYLA(iNCCYLA);
+			scheduleCYLA.setBusProfExclSpecProf(busProfExclSpecProf);
+		}
+		//end
 
 		STCG sTCG = new STCG();
 		IncCYLA incCYLAforSTCG = new IncCYLA();
@@ -84,7 +133,7 @@ public class CurrentYearLossesSchedules extends XmlCalculation{
 		}else
 			incCYLAforSTCG.setIncOfCurYrUnderThatHead(new BigInteger("0"));
 		incCYLAforSTCG.setHPlossCurYrSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedHouseIncomeAftSTC").toString())));
-		incCYLAforSTCG.setBusLossSetoff(new BigInteger("0"));
+		incCYLAforSTCG.setBusLossSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedBusinessIncomeAftSTC").toString())));
 		incCYLAforSTCG.setOthSrcLossNoRaceHorseSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedOtherIncomeAftSTC").toString())));
 		incCYLAforSTCG.setIncOfCurYrAfterSetOff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("adjustedSTCGain").toString())));
 		sTCG.setIncCYLA(incCYLAforSTCG);
@@ -97,7 +146,7 @@ public class CurrentYearLossesSchedules extends XmlCalculation{
 			incCYLAforLTCG.setIncOfCurYrUnderThatHead(indianCurrencyHelper.longToBigInteger(LTCGain));
 		}else
 			incCYLAforLTCG.setIncOfCurYrUnderThatHead(new BigInteger("0"));
-		incCYLAforLTCG.setBusLossSetoff(new BigInteger("0"));
+		incCYLAforLTCG.setBusLossSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedBusinessIncomeAftLTC").toString())));
 		incCYLAforLTCG.setHPlossCurYrSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedHouseIncomeAftLTC").toString())));
 		incCYLAforLTCG.setOthSrcLossNoRaceHorseSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedOtherIncomeAftLTC").toString())));
 		incCYLAforLTCG.setIncOfCurYrAfterSetOff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("adjustedLTCGain").toString())));
@@ -110,7 +159,7 @@ public class CurrentYearLossesSchedules extends XmlCalculation{
 			incCYLAforOtherRaceHorse.setIncOfCurYrUnderThatHead(indianCurrencyHelper.longToBigInteger(otherincome));
 		}else
 			incCYLAforOtherRaceHorse.setIncOfCurYrUnderThatHead(new BigInteger("0"));
-		incCYLAforOtherRaceHorse.setBusLossSetoff(new BigInteger("0"));
+		incCYLAforOtherRaceHorse.setBusLossSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedBusinessIncomeAftOI").toString())));
 		incCYLAforOtherRaceHorse.setHPlossCurYrSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedHouseIncomeAftOI").toString())));
 		incCYLAforOtherRaceHorse.setOthSrcLossNoRaceHorseSetoff(new BigInteger("0"));
 		incCYLAforOtherRaceHorse.setIncOfCurYrAfterSetOff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("adjustedOtherIncome").toString())));
@@ -124,7 +173,7 @@ public class CurrentYearLossesSchedules extends XmlCalculation{
 			incCYLAforRaceHorse.setIncOfCurYrUnderThatHead(indianCurrencyHelper.longToBigInteger(SrcRaceHorse));
 		}else
 			incCYLAforRaceHorse.setIncOfCurYrUnderThatHead(new BigInteger("0"));
-		incCYLAforRaceHorse.setBusLossSetoff(new BigInteger("0"));
+		incCYLAforRaceHorse.setBusLossSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedBusinessIncomeAftHR").toString())));
 		incCYLAforRaceHorse.setHPlossCurYrSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedHouseIncomeAftRH").toString())));
 		incCYLAforRaceHorse.setOthSrcLossNoRaceHorseSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("balancedOtherIncomeAftHR").toString())));
 		incCYLAforRaceHorse.setIncOfCurYrAfterSetOff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("adjustedmaintainingRaceHorseIncome").toString())));
@@ -133,10 +182,13 @@ public class CurrentYearLossesSchedules extends XmlCalculation{
 
 		TotalCurYr totalCurYr = new TotalCurYr();
 		if(houseIncomeTotal<0){
-		totalCurYr.setTotHPlossCurYr(indianCurrencyHelper.longToBigInteger(houseIncomeTotal));
+			totalCurYr.setTotHPlossCurYr(indianCurrencyHelper.longToBigInteger(houseIncomeTotal));
 		}else
 			totalCurYr.setTotHPlossCurYr(new BigInteger("0"));
-		totalCurYr.setTotBusLoss(new BigInteger("0"));
+		if(businessIncome < 0){
+			totalCurYr.setTotBusLoss(indianCurrencyHelper.longToBigInteger(businessIncome));
+		}else
+			totalCurYr.setTotBusLoss(new BigInteger("0"));
 		if(otherincome<0){
 			totalCurYr.setTotOthSrcLossNoRaceHorse(indianCurrencyHelper.longToBigInteger(otherincome));
 		}else
@@ -145,13 +197,13 @@ public class CurrentYearLossesSchedules extends XmlCalculation{
 
 		TotalLossSetOff totalLossSetOff = new TotalLossSetOff();
 		totalLossSetOff.setTotHPlossCurYrSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("totalHPLossSetoff").toString())));
-		totalLossSetOff.setTotBusLossSetoff(new BigInteger("0"));
+		totalLossSetOff.setTotBusLossSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("totalBILossSetoff").toString())));
 		totalLossSetOff.setTotOthSrcLossNoRaceHorseSetoff(indianCurrencyHelper.bigIntegerRound(Double.parseDouble(resultMapLosses.get("totalOILossSetoff").toString())));
 		scheduleCYLA.setTotalLossSetOff(totalLossSetOff);
 
 		LossRemAftSetOff lossRemAftSetOff = new LossRemAftSetOff();
 		lossRemAftSetOff.setBalHPlossCurYrAftSetoff(totalCurYr.getTotHPlossCurYr().subtract(totalLossSetOff.getTotHPlossCurYrSetoff()));
-		lossRemAftSetOff.setBalBusLossAftSetoff(new BigInteger("0"));
+		lossRemAftSetOff.setBalBusLossAftSetoff(totalCurYr.getTotBusLoss().subtract(totalLossSetOff.getTotBusLossSetoff()));
 		lossRemAftSetOff.setBalOthSrcLossNoRaceHorseAftSetoff(totalCurYr.getTotOthSrcLossNoRaceHorse().subtract(totalLossSetOff.getTotOthSrcLossNoRaceHorseSetoff()));
 		scheduleCYLA.setLossRemAftSetOff(lossRemAftSetOff);
 
