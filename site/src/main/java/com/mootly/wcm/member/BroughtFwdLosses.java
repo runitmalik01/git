50510bff56de70ffb9a8abf6c59381466c4d7f74
Dhananjay <dpanwar@mootly.com>
2013-10-11 11:36:40 +0000

    changes made for BroughtFwdLosses.java

--- site/src/main/java/com/mootly/wcm/member/BroughtFwdLosses.java
+++ site/src/main/java/com/mootly/wcm/member/BroughtFwdLosses.java
@@ -1,6 +1,14 @@
 package com.mootly.wcm.member;
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import in.gov.incometaxindiaefiling.y2012_2013.ITR;
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleCYLA;
+
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.core.component.HstComponentException;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
@@ -9,8 +17,23 @@ import org.slf4j.LoggerFactory;
 
 import com.mootly.wcm.annotations.FormFields;
 import com.mootly.wcm.annotations.PrimaryBean;
+import com.mootly.wcm.beans.AdjustmentOfLossesDoc;
 import com.mootly.wcm.beans.BroughtFwdLossesDocument;
+import com.mootly.wcm.beans.DeductionSchedTenADocumemt;
+import com.mootly.wcm.beans.FormSixteenDocument;
+import com.mootly.wcm.beans.HouseProperty;
+import com.mootly.wcm.beans.IncBusinessProfessionDoc;
+import com.mootly.wcm.beans.MemberPersonalInformation;
+import com.mootly.wcm.beans.OtherInformationDocument;
+import com.mootly.wcm.beans.OtherSourcesDocument;
+import com.mootly.wcm.beans.ProfitAndLossDocument;
+import com.mootly.wcm.beans.SalaryIncomeDocument;
+import com.mootly.wcm.beans.ScheduleDOADocument;
+import com.mootly.wcm.beans.ScheduleDPMDocument;
+import com.mootly.wcm.beans.ScheduleESRDocument;
+import com.mootly.wcm.beans.compound.AdjustmentOfLossesCom;
 import com.mootly.wcm.components.ITReturnComponent;
+import com.mootly.wcm.model.schedules.y2012_2013.CurrentYearLossesSchedules;
 
 /*
  * Author:Dhananjay Panwar
@@ -35,6 +58,116 @@ public class BroughtFwdLosses extends ITReturnComponent {
 	public void doBeforeRender(HstRequest request, HstResponse response) {
 		// TODO Auto-generated method stub
 		super.doBeforeRender(request, response);
+
+		MemberPersonalInformation memberPersonalInformation = (MemberPersonalInformation) request.getAttribute(MemberPersonalInformation.class.getSimpleName().toLowerCase());
+		SalaryIncomeDocument salaryIncomeDocument = (SalaryIncomeDocument) request.getAttribute(SalaryIncomeDocument.class.getSimpleName().toLowerCase());
+		FormSixteenDocument formSixteenDocument = (FormSixteenDocument) request.getAttribute(FormSixteenDocument.class.getSimpleName().toLowerCase());
+		HouseProperty houseProperty = (HouseProperty) request.getAttribute(HouseProperty.class.getSimpleName().toLowerCase());
+		OtherSourcesDocument otherSourcesDocument = (OtherSourcesDocument) request.getAttribute(OtherSourcesDocument.class.getSimpleName().toLowerCase());
+		ScheduleDPMDocument scheduleDPMDocument = (ScheduleDPMDocument) request.getAttribute(ScheduleDPMDocument.class.getSimpleName().toLowerCase());
+		ScheduleDOADocument scheduleDOADocument = (ScheduleDOADocument) request.getAttribute(ScheduleDOADocument.class.getSimpleName().toLowerCase());
+		DeductionSchedTenADocumemt deductionSchedTenADocumemt = (DeductionSchedTenADocumemt) request.getAttribute(DeductionSchedTenADocumemt.class.getSimpleName().toLowerCase());
+		OtherInformationDocument otherInformationDocument = (OtherInformationDocument) request.getAttribute(OtherInformationDocument.class.getSimpleName().toLowerCase());
+		ProfitAndLossDocument profitAndLossDocument = (ProfitAndLossDocument) request.getAttribute(ProfitAndLossDocument.class.getSimpleName().toLowerCase());
+		IncBusinessProfessionDoc incBusinessProfessionDoc = (IncBusinessProfessionDoc) request.getAttribute(IncBusinessProfessionDoc.class.getSimpleName().toLowerCase());
+		ScheduleESRDocument scheduleESRDocument = (ScheduleESRDocument) request.getAttribute(ScheduleESRDocument.class.getSimpleName().toLowerCase());
+		AdjustmentOfLossesDoc adjustmentOfLossesDoc = (AdjustmentOfLossesDoc) request.getAttribute(AdjustmentOfLossesDoc.class.getSimpleName().toLowerCase());
+
+		ITR itr = new ITR();
+		Map<String, HippoBean> inputBeans = new HashMap<String, HippoBean>();
+		inputBeans.put(MemberPersonalInformation.class.getSimpleName().toLowerCase(),memberPersonalInformation);
+		inputBeans.put(SalaryIncomeDocument.class.getSimpleName().toLowerCase(),salaryIncomeDocument);
+		inputBeans.put(FormSixteenDocument.class.getSimpleName().toLowerCase(),formSixteenDocument);
+		inputBeans.put(HouseProperty.class.getSimpleName().toLowerCase(),houseProperty);
+		inputBeans.put(OtherSourcesDocument.class.getSimpleName().toLowerCase(),otherSourcesDocument);
+		inputBeans.put(AdjustmentOfLossesDoc.class.getSimpleName().toLowerCase(),adjustmentOfLossesDoc);
+		inputBeans.put(OtherInformationDocument.class.getSimpleName().toLowerCase(),otherInformationDocument);
+		inputBeans.put(ProfitAndLossDocument.class.getSimpleName().toLowerCase(),profitAndLossDocument);
+		inputBeans.put(ScheduleESRDocument.class.getSimpleName().toLowerCase(),scheduleESRDocument);
+		inputBeans.put(DeductionSchedTenADocumemt.class.getSimpleName().toLowerCase(),deductionSchedTenADocumemt);
+		inputBeans.put(IncBusinessProfessionDoc.class.getSimpleName().toLowerCase(),incBusinessProfessionDoc);
+		inputBeans.put(ScheduleDPMDocument.class.getSimpleName().toLowerCase(),scheduleDPMDocument);
+		inputBeans.put(ScheduleDOADocument.class.getSimpleName().toLowerCase(),scheduleDOADocument);
+
+		CurrentYearLossesSchedules currentYearLossesSchedules = new CurrentYearLossesSchedules();
+		ScheduleCYLA scheduleCYLA = currentYearLossesSchedules.getScheduleCYLA(itr, getFinancialYear(), inputBeans);
+
+		request.setAttribute("SalaryIncomeAftSetOff", scheduleCYLA.getSalary().getIncCYLA().getIncOfCurYrAfterSetOff());
+		request.setAttribute("HPIncomeAftSetOff", scheduleCYLA.getHP().getIncCYLA().getIncOfCurYrAfterSetOff());
+		request.setAttribute("BusinessIncomeAftSetOff", scheduleCYLA.getBusProfExclSpecProf().getIncCYLA().getIncOfCurYrAfterSetOff());
+		request.setAttribute("SpeculativeIncomeAftSetOff", scheduleCYLA.getSpeculativeInc().getIncCYLA().getIncOfCurYrAfterSetOff());
+		request.setAttribute("SpecifiedIncomeAftSetOff", scheduleCYLA.getSpecifiedInc().getIncCYLA().getIncOfCurYrAfterSetOff());
+		request.setAttribute("STCGIncomeAftSetOff", scheduleCYLA.getSTCG().getIncCYLA().getIncOfCurYrAfterSetOff());
+		request.setAttribute("LTCGIncomeAftSetOff", scheduleCYLA.getLTCG().getIncCYLA().getIncOfCurYrAfterSetOff());
+		request.setAttribute("OtherIncomeAftSetOff", scheduleCYLA.getOthSrcExclRaceHorse().getIncCYLA().getIncOfCurYrAfterSetOff());
+		request.setAttribute("RaceHorseIncomeAftSetOff", scheduleCYLA.getOthSrcRaceHorse().getIncCYLA().getIncOfCurYrAfterSetOff());
+
+		Double hpLoss = 0d; Double ltcLoss = 0d; Double stcLoss = 0d; Double maintainingRaceHorseLoss = 0d;
+		Double lossFrmSpecifiedBuss = 0d; Double nonSpeculationBusinessLoss = 0d; Double speculationBusinessLoss = 0d;
+
+		if(adjustmentOfLossesDoc != null){
+			List<AdjustmentOfLossesCom> listofAdjustmentOfLossesCom = adjustmentOfLossesDoc.getAdjustmentOfLossesList() ;
+			if ( listofAdjustmentOfLossesCom != null && listofAdjustmentOfLossesCom.size() > 0 ){
+				for(AdjustmentOfLossesCom adjustmentOfLossesCom:listofAdjustmentOfLossesCom){
+					if(adjustmentOfLossesCom.getNameOfHead().equals("House Property Loss")){
+						hpLoss =hpLoss + adjustmentOfLossesCom.getAmount();
+					}
+					if(adjustmentOfLossesCom.getNameOfHead().equals("Long Term Capital Loss")){
+						ltcLoss = ltcLoss + adjustmentOfLossesCom.getAmount();
+					}
+					if(adjustmentOfLossesCom.getNameOfHead().equals("Short Term Capital Loss")){
+						stcLoss = stcLoss + adjustmentOfLossesCom.getAmount();
+					}
+					if(adjustmentOfLossesCom.getNameOfHead().equals("Owning and Maintaining Race Horses")){
+						maintainingRaceHorseLoss = maintainingRaceHorseLoss + adjustmentOfLossesCom.getAmount();
+					}
+					//added for ITR4
+					if(adjustmentOfLossesCom.getNameOfHead().equals("Loss From Specified Business")){
+						lossFrmSpecifiedBuss = lossFrmSpecifiedBuss + adjustmentOfLossesCom.getAmount();
+					}
+					if(adjustmentOfLossesCom.getNameOfHead().equals("Non Speculation Business Loss")){
+						nonSpeculationBusinessLoss = nonSpeculationBusinessLoss + adjustmentOfLossesCom.getAmount();
+					}
+					if(adjustmentOfLossesCom.getNameOfHead().equals("Speculation Business Loss")){
+						speculationBusinessLoss = speculationBusinessLoss + adjustmentOfLossesCom.getAmount();
+					}
+				}
+			}
+		}
+		if(scheduleCYLA.getHP().getIncCYLA().getIncOfCurYrAfterSetOff().doubleValue()>hpLoss){
+			request.setAttribute("hpLoss", hpLoss);
+		}else
+			request.setAttribute("hpLoss", scheduleCYLA.getHP().getIncCYLA().getIncOfCurYrAfterSetOff());
+
+		if(scheduleCYLA.getBusProfExclSpecProf().getIncCYLA().getIncOfCurYrAfterSetOff().doubleValue()>nonSpeculationBusinessLoss){
+			request.setAttribute("nonSpeculationBusinessLoss", nonSpeculationBusinessLoss);
+		}else
+			request.setAttribute("nonSpeculationBusinessLoss", scheduleCYLA.getBusProfExclSpecProf().getIncCYLA().getIncOfCurYrAfterSetOff());
+
+		if(scheduleCYLA.getSpeculativeInc().getIncCYLA().getIncOfCurYrAfterSetOff().doubleValue()>speculationBusinessLoss){
+			request.setAttribute("speculationBusinessLoss", speculationBusinessLoss);
+		}else
+			request.setAttribute("speculationBusinessLoss", scheduleCYLA.getSpeculativeInc().getIncCYLA().getIncOfCurYrAfterSetOff());
+
+		if(scheduleCYLA.getSpecifiedInc().getIncCYLA().getIncOfCurYrAfterSetOff().doubleValue()>lossFrmSpecifiedBuss){
+			request.setAttribute("lossFrmSpecifiedBuss", lossFrmSpecifiedBuss);
+		}else
+			request.setAttribute("lossFrmSpecifiedBuss", scheduleCYLA.getSpecifiedInc().getIncCYLA().getIncOfCurYrAfterSetOff());
+
+		if(scheduleCYLA.getSTCG().getIncCYLA().getIncOfCurYrAfterSetOff().doubleValue()>stcLoss){
+			request.setAttribute("stcLoss", stcLoss);
+		}else
+			request.setAttribute("stcLoss", scheduleCYLA.getSTCG().getIncCYLA().getIncOfCurYrAfterSetOff());
+
+		if(scheduleCYLA.getLTCG().getIncCYLA().getIncOfCurYrAfterSetOff().doubleValue()>ltcLoss){
+			request.setAttribute("ltcLoss", ltcLoss);
+		}else
+			request.setAttribute("ltcLoss", scheduleCYLA.getLTCG().getIncCYLA().getIncOfCurYrAfterSetOff());
+
+		if(scheduleCYLA.getOthSrcRaceHorse().getIncCYLA().getIncOfCurYrAfterSetOff().doubleValue()>maintainingRaceHorseLoss){
+			request.setAttribute("maintainingRaceHorseLoss", maintainingRaceHorseLoss);
+		}else
+			request.setAttribute("maintainingRaceHorseLoss", scheduleCYLA.getOthSrcRaceHorse().getIncCYLA().getIncOfCurYrAfterSetOff());
 	}
 
 	@Override
