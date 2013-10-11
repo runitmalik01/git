bd2427ae782ae2b491890a746ad5536d57ccb6f1
Dhananjay <dpanwar@mootly.com>
2013-10-11 17:52:38 +0000

    added Form_ITR3.java for itr3

---
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/Form_ITR3.java
@@ -0,0 +1,18 @@
+package com.mootly.wcm.model.schedules.y2012_2013;
+
+import in.gov.incometaxindiaefiling.y2012_2013.FormITR3;
+import in.gov.incometaxindiaefiling.y2012_2013.ITR;
+
+public class Form_ITR3 {
+
+	public FormITR3 getFormITR3(ITR itr){
+
+		FormITR3 formITR3 = new FormITR3();
+		formITR3.setFormName("ITR-3");
+		formITR3.setDescription(" ");
+		formITR3.setAssessmentYear("2013");
+		formITR3.setSchemaVer("Ver1.0");
+		formITR3.setFormVer("Ver1.0");
+		return formITR3;
+	}
+}
