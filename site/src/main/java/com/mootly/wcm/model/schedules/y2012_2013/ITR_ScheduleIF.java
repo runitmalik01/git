881b596cd0fb193e5055368e98f8de2ca8810e8e
Dhananjay <dpanwar@mootly.com>
2013-10-15 18:46:04 +0000

    changes made for ITR_ScheduleIF.java

--- site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ITR_ScheduleIF.java
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/ITR_ScheduleIF.java
@@ -7,20 +7,24 @@ import in.gov.incometaxindiaefiling.y2012_2013.ScheduleIF;
 import in.gov.incometaxindiaefiling.y2012_2013.ScheduleIF.PartnerFirmDetails;
 
 import com.mootly.wcm.beans.FirmsPartnerDocument;
+import com.mootly.wcm.beans.MemberPersonalInformation;
 import com.mootly.wcm.beans.compound.FirmsPartnerDetail;
 
 public class ITR_ScheduleIF {
 
 	FirmsPartnerDocument firmsPartnerDocument = null;
+	MemberPersonalInformation memberPersonalInformation = null;
 
-	public ITR_ScheduleIF(FirmsPartnerDocument firmsPartnerDocument){
+	public ITR_ScheduleIF(FirmsPartnerDocument firmsPartnerDocument, MemberPersonalInformation memberPersonalInformation){
 		this.firmsPartnerDocument = firmsPartnerDocument;
+		this.memberPersonalInformation = memberPersonalInformation;
 	}
 
 	public ScheduleIF getScheduleIF(ITR itr){
 
 		ScheduleIF scheduleIF = new ScheduleIF();
 		boolean hasAValidFirm = false;
+		String itrSelection =  memberPersonalInformation.getFlexField("flex_string_ITRForm", "");
 
 		long totPrftShr = 0;
 		long totFrmCapBal = 0;
@@ -31,6 +35,9 @@ public class ITR_ScheduleIF {
 					PartnerFirmDetails partnerFirmDetails = new PartnerFirmDetails();
 					partnerFirmDetails.setFirmName(firmsPartnerDetail.getName_Firm());
 					partnerFirmDetails.setFirmPAN(firmsPartnerDetail.getPan_Firm());
+					if(itrSelection.equals("ITR3")){
+						partnerFirmDetails.setIsLiableToAudit(firmsPartnerDetail.getIsLiableToAudit());
+					}
 					partnerFirmDetails.setProfitSharePercent(Double.parseDouble(firmsPartnerDetail.getPerShare_InProfit()));
 					partnerFirmDetails.setProfitShareAmt(firmsPartnerDetail.getAmountShare_InProfit().longValue());
 					totPrftShr = totPrftShr + firmsPartnerDetail.getAmountShare_InProfit().longValue();
