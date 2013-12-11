b331c5ce536ac61805610e5913d4a0175aab9a2a
Pankaj <psingh@mootly.com>
2013-12-11 11:55:28 +0000

    scheedule STTR

---
+++ site/src/main/java/com/mootly/wcm/model/schedules/y2012_2013/Schedule_STTR.java
@@ -0,0 +1,28 @@
+package com.mootly.wcm.model.schedules.y2012_2013;
+
+import java.math.BigInteger;
+
+import com.mootly.wcm.services.IndianCurrencyHelper;
+
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleSTTR;
+import in.gov.incometaxindiaefiling.y2012_2013.ScheduleSTTR.IncChrgSTT;
+
+public class Schedule_STTR {
+	
+	ScheduleSTTR scheduleSTTR = new ScheduleSTTR();
+	IndianCurrencyHelper indianCurrencyHelper = new IndianCurrencyHelper();
+	IncChrgSTT incChrgSTT = new IncChrgSTT();
+
+	public ScheduleSTTR getScheduleSTTR(){
+		scheduleSTTR.setRebateUs88E(indianCurrencyHelper.bigIntegerRoundStr("0"));
+		scheduleSTTR.setSTTPaid(indianCurrencyHelper.bigIntegerRoundStr("0"));
+		scheduleSTTR.setTaxPaySTTAvgRate(indianCurrencyHelper.bigIntegerRoundStr("0"));
+		IncChrgSTT incChrgSTT = new IncChrgSTT();
+		incChrgSTT.setSTTNonSpecBus(indianCurrencyHelper.bigIntegerRoundStr("0"));
+		incChrgSTT.setSTTSpecBus(new BigInteger("0"));
+		incChrgSTT.setTotChrgAmtSTT(new BigInteger("0"));
+		scheduleSTTR.setIncChrgSTT(incChrgSTT);
+		return scheduleSTTR;
+	}
+
+}
