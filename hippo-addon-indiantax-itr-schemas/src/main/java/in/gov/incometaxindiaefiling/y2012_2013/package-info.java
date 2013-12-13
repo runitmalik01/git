be5e8a41c9c5566b2c833c874d6171b7e70d9aec
mootlygithub <amit@mootly.com>
2013-12-13 02:06:25 +0000

    Second clean up

--- hippo-addon-indiantax-itr-schemas/src/main/java/in/gov/incometaxindiaefiling/y2012_2013/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://incometaxindiaefiling.gov.in/master", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package in.gov.incometaxindiaefiling.y2012_2013;
