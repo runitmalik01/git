7dde46e035d2d8efc40cd58fb92ab44d745aa068
mootlygithub <amit@mootly.com>
2013-12-13 01:52:23 +0000

    Clean Delete

--- hippo-addon-indiantax-itr-schemas/src/main/java/in/gov/incometaxindiaefiling/y2012_2013/corp/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://incometaxindiaefiling.gov.in/Corpmaster", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package in.gov.incometaxindiaefiling.y2012_2013.corp;
