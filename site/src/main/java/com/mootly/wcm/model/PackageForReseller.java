ec216efccd1bc3280ff6636f36a0e037986f104d
Dhananjay <dpanwar@mootly.com>
2013-12-18 18:54:56 +0000

    added PackageForReseller.java

---
+++ site/src/main/java/com/mootly/wcm/model/PackageForReseller.java
@@ -0,0 +1,30 @@
+
+package com.mootly.wcm.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum PackageForReseller {
+
+	trailperiod("Trail Period (7 Days)"),
+	paid_singleuser("Single User"),
+	paid_multipleuser("Multiple User");
+
+	String displayName;//name of question 
+
+	PackageForReseller(String displayName) {
+		this.displayName= displayName;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public static List<PackageForReseller> getResellerPackage(){
+		List<PackageForReseller> resellerPackage = new ArrayList<PackageForReseller>();
+		for(PackageForReseller rp:PackageForReseller.values()){
+			resellerPackage.add(rp);
+		}
+		return resellerPackage;
+	}
+}
