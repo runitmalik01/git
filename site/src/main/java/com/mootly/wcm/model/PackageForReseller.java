a3167d53b3a0c2c2d835923ad90e828d2d7098b2
Dhananjay <dpanwar@mootly.com>
2013-12-20 16:34:17 +0000

    changes made for PackageForReseller.java

--- site/src/main/java/com/mootly/wcm/model/PackageForReseller.java
+++ site/src/main/java/com/mootly/wcm/model/PackageForReseller.java
@@ -6,17 +6,32 @@ import java.util.List;
 
 public enum PackageForReseller {
 
-	paid_singleuser("Single User"),
-	paid_multipleuser("Multiple User");
+	singleUser("Single User",10000,"1","1 Year"),
+	multipleUser("Multiple User",40000,"unlimited","1 Year");
 
-	String displayName;//name of question 
+	String displayName;//name of Package
+	int amount;// Package cost
+	String numberOfLicensedUsers;//How many users can signup 
+	String vadilityOfLicense;
 
-	PackageForReseller(String displayName) {
-		this.displayName= displayName;
+	PackageForReseller(String displayName,int amount,String numberOfLicensedUsers,String vadilityOfLicense) {
+		this.displayName = displayName;
+		this.amount = amount;
+		this.numberOfLicensedUsers = numberOfLicensedUsers;
+		this.vadilityOfLicense = vadilityOfLicense;
 	}
 
 	public String getDisplayName() {
 		return displayName;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public String getNumberOfLicensedUsers() {
+		return numberOfLicensedUsers;
+	}
+	public String getVadilityOfLicense() {
+		return vadilityOfLicense;
 	}
 
 	public static List<PackageForReseller> getResellerPackage(){
