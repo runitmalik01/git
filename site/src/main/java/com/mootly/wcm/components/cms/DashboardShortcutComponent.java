af9ef0649380ba29aa53238a1134bed90e6d9da4
Priyank Gupta <pgupta@mootly.com>
2013-12-24 05:35:33 +0000

    added for dashboard shortcut

---
+++ site/src/main/java/com/mootly/wcm/components/cms/DashboardShortcutComponent.java
@@ -0,0 +1,143 @@
+/**
+ * 
+ */
+package com.mootly.wcm.components.cms;
+
+import java.io.IOException;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.hippoecm.hst.component.support.forms.FormMap;
+import org.hippoecm.hst.content.beans.ObjectBeanManagerException;
+import org.hippoecm.hst.content.beans.ObjectBeanPersistenceException;
+import org.hippoecm.hst.content.beans.manager.workflow.WorkflowCallbackHandler;
+import org.hippoecm.hst.content.beans.manager.workflow.WorkflowPersistenceManager;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoFolder;
+import org.hippoecm.hst.core.component.HstComponentException;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.hippoecm.hst.core.linking.HstLink;
+import org.hippoecm.repository.reviewedactions.FullReviewedActionsWorkflow;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mootly.wcm.annotations.FormFields;
+import com.mootly.wcm.annotations.RequiredFields;
+import com.mootly.wcm.beans.cms.DashboardShortcutDocument;
+import com.mootly.wcm.components.ITReturnComponent;
+import com.mootly.wcm.model.IndianGregorianCalendar;
+
+/**
+ * @author BEN-10
+ *
+ */
+@FormFields(fieldNames={"documentName","documentType","contentFolder"})
+@RequiredFields(fieldNames={"documentName","documentType","contentFolder"})
+
+public class DashboardShortcutComponent extends ITReturnComponent {
+
+	public static final Logger log = LoggerFactory.getLogger(DashboardShortcutComponent.class);
+	public static final String DASHBOARD = "dashboard";
+	public static final String CONTENT_FOLDER = "contentFolder";
+	public static final String DOCUMENT_TYPE = "documentType";
+	public static final String DOCUMENT_NAME = "documentName";
+	public static final String REDIRECT_REF_ID = "global-document-editor";
+
+	@Override
+	public void doBeforeRender(HstRequest request, HstResponse response) {
+		// TODO Auto-generated method stub
+		super.doBeforeRender(request, response);
+		HippoBean siteContentBean = getSiteContentBaseBean(request);
+		HippoBean dashboardContentBean = siteContentBean.getBean(DASHBOARD);
+		if(dashboardContentBean == null){
+			if(log.isInfoEnabled()){
+				log.info("Scope of dashboard content Bean is { } null or not able to fetched.");
+			}
+			return;
+		}
+		if(dashboardContentBean instanceof HippoFolder){
+			HippoFolder dashboardFolder = (HippoFolder) dashboardContentBean;
+			List<DashboardShortcutDocument> documents = dashboardFolder.getDocuments(DashboardShortcutDocument.class);
+			if(log.isInfoEnabled()){
+				log.info("Lets ceck the Dashboard Document in list"+documents.size());
+			}
+			request.setAttribute("documents", documents);
+		} 
+	}
+
+	@Override
+	public void doAction(HstRequest request, HstResponse response)
+			throws HstComponentException {
+		// TODO Auto-generated method stub
+		super.doAction(request, response);
+		HippoBean resellerSiteHippoBean = getSiteContentBaseBeanForReseller(request);
+		HstLink hstLink = request.getRequestContext().getHstLinkCreator().createByRefId(REDIRECT_REF_ID, request.getRequestContext().getResolvedMount().getMount());
+		String redirectURL = hstLink.toUrlForm(request.getRequestContext(), true);
+		String basePathForDocument = resellerSiteHippoBean.getCanonicalPath();
+		FormMap dashboardFormMap = getFormMap();
+		if(dashboardFormMap != null){
+			String contentFolder = dashboardFormMap.getField(CONTENT_FOLDER).getValue();
+			String documentType = dashboardFormMap.getField(DOCUMENT_TYPE).getValue();
+			String documentName = dashboardFormMap.getField(DOCUMENT_NAME).getValue();
+			Session persistabSession = null;
+			WorkflowPersistenceManager wpm = null;
+			try {
+				persistabSession = getPersistableSession(request);
+				wpm = getWorkflowPersistenceManager(persistabSession);
+				wpm.setWorkflowCallbackHandler(new FullReviewedWorkflowCallbackHandler());
+				String absolutePathForDocument = CreateAbsolutePathForDocument(request, basePathForDocument, contentFolder);
+				if(log.isInfoEnabled()){
+					log.info("Lets check path for Document::"+absolutePathForDocument);
+				}
+				String absolutePathOfCreated = wpm.createAndReturn(absolutePathForDocument, documentType, documentName, true);
+				Object object = wpm.getObject(absolutePathOfCreated);
+				if(object instanceof HippoBean){
+					//HippoBean documentBean = (HippoBean) object;
+					if(log.isInfoEnabled()){
+						log.info("Lets check path for Redirect::"+redirectURL);
+					}
+					//wpm.update(object);
+					response.sendRedirect(redirectURL+absolutePathOfCreated+".html");
+				}
+			} catch (RepositoryException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ObjectBeanPersistenceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ObjectBeanManagerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	/**
+	 * This method is used to create the Absolute Path for Document.
+	 * 
+	 * @param request {@link HstRequest}
+	 * @param basePathForDocument {@link String}
+	 * @param contentFolder {@link String}
+	 * */
+
+	@SuppressWarnings("deprecation")
+	public String CreateAbsolutePathForDocument(HstRequest request, String basePathForDocument, String contentFolder){
+		StringBuilder stringBuilderForPath = new StringBuilder();
+		stringBuilderForPath.append(basePathForDocument).append('/').append(contentFolder);
+		GregorianCalendar indianCalendar = IndianGregorianCalendar.getCurrentDateInIndiaAsDate();
+		stringBuilderForPath.append('/').append(indianCalendar.getTime().getYear()+1990).append('/').append(indianCalendar.getTime().getMonth()+1);
+		return stringBuilderForPath.toString();
+	}
+
+	public static class FullReviewedWorkflowCallbackHandler implements WorkflowCallbackHandler<FullReviewedActionsWorkflow> {
+		public void processWorkflow(FullReviewedActionsWorkflow wf) throws Exception {
+			wf.publish();
+		}
+	}
+}
---
+++ site/src/main/java/com/mootly/wcm/components/cms/DocumentUpdateComponent.java
@@ -0,0 +1,182 @@
+/**
+ * 
+ */
+package com.mootly.wcm.components.cms;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.apache.commons.lang.StringUtils;
+import org.hippoecm.hst.component.support.forms.FormMap;
+import org.hippoecm.hst.content.beans.ObjectBeanManagerException;
+import org.hippoecm.hst.content.beans.manager.workflow.WorkflowPersistenceManager;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.core.component.HstComponentException;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mootly.wcm.annotations.AutoCreateFormField;
+import com.mootly.wcm.beans.ValueListDocument;
+import com.mootly.wcm.components.ITReturnComponent;
+import com.mootly.wcm.components.cms.view.AbstractFormField;
+import com.mootly.wcm.services.CustomContentNodeBinderImpl;
+
+/**
+ * @author BEN-10
+ *
+ */
+public class DocumentUpdateComponent extends ITReturnComponent {
+
+	public static final Logger log = LoggerFactory.getLogger(DocumentUpdateComponent.class);
+
+	@Override
+	public void doBeforeRender(HstRequest request, HstResponse response) {
+		// TODO Auto-generated method stub
+		super.doBeforeRender(request, response);
+		HippoBean siteContentBaseBean = getSiteContentBaseBeanForReseller(request);
+		if(siteContentBaseBean == null){
+			if(log.isInfoEnabled()){
+				log.info("Site content Bean is { } null.Not able to proceed.");
+			}
+			return;
+		}	
+		Map<String,AbstractFormField> mapOfAbstractFormFields = new HashMap<String, AbstractFormField>();
+		String documentCanonicalPath = request.getRequestContext().getResolvedSiteMapItem().getParameter("documentCanonicalPath");
+		if(StringUtils.isNotBlank(documentCanonicalPath)){
+			if(!documentCanonicalPath.startsWith("/")){
+				documentCanonicalPath = "/"+documentCanonicalPath;
+			}
+			if(log.isInfoEnabled()){
+				log.info("Lets see Path of Document::"+documentCanonicalPath);
+			}
+			Session persistableSession = null;
+			WorkflowPersistenceManager wpm = null;
+			try {
+				persistableSession = getPersistableSession(request);
+				wpm = getWorkflowPersistenceManager(persistableSession);
+				wpm.setWorkflowCallbackHandler(new FullReviewedWorkflowCallbackHandler());
+				Object object = wpm.getObject(documentCanonicalPath);
+				if(log.isInfoEnabled()){
+					log.info("have the object and Name of object"+object.toString());
+				}
+				if(object instanceof HippoBean){
+					HippoBean hippoBeanDocument = (HippoBean) object;
+					Method[] hippoBeanDocumentMethods = hippoBeanDocument.getClass().getMethods();//getAnnotation(AutoCreateFormField.class);
+					for(Method beanDocumentMethod:hippoBeanDocumentMethods){
+						if(beanDocumentMethod.isAnnotationPresent(AutoCreateFormField.class)){
+							AutoCreateFormField createFormField = beanDocumentMethod.getAnnotation(AutoCreateFormField.class);
+							ValueListDocument valueListDocument = null;
+							if(StringUtils.isNotBlank(createFormField.valueListName())){
+								//HippoBean scopeBaseBean = getSiteContentBaseBean(request);
+								if(log.isInfoEnabled()){
+									log.info("Value List Document"+createFormField.valueListName());
+								}
+								String baseValueListContentPath = request.getRequestContext().getResolvedMount().getMount().getCanonicalContentPath()+"/common/valuelists/";
+								valueListDocument = (ValueListDocument) getObjectBeanManager(request).getObject(baseValueListContentPath+createFormField.valueListName());
+							}
+							if(log.isInfoEnabled()){
+								log.info("Lets check this property in Document Bean");
+							}
+							Object objectProperty = beanDocumentMethod.invoke(hippoBeanDocument,new Object[]{});//hippoBeanDocument.getProperty("mootlywcm:"+beanDocumentMethod.getName());
+							AbstractFormField abstractFormField = new AbstractFormField(createFormField, valueListDocument, objectProperty);
+							mapOfAbstractFormFields.put(createFormField.name(), abstractFormField);
+						}
+					}
+					request.setAttribute("hippoBeanDocument", hippoBeanDocument);
+					request.setAttribute("mapOfAbstractFormFields", mapOfAbstractFormFields);
+				}
+			} catch (RepositoryException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ObjectBeanManagerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void doAction(HstRequest request, HstResponse response)
+			throws HstComponentException {
+		// TODO Auto-generated method stub
+		//super.doAction(request, response);
+		String documentCanonicalPath = request.getRequestContext().getResolvedSiteMapItem().getParameter("documentCanonicalPath");
+		if(StringUtils.isNotBlank(documentCanonicalPath)){
+			if(!documentCanonicalPath.startsWith("/")){
+				documentCanonicalPath = "/"+documentCanonicalPath;
+			}
+			if(log.isInfoEnabled()){
+				log.info("Lets see Path of Document::"+documentCanonicalPath);
+			}
+			Session persistableSession = null;
+			WorkflowPersistenceManager wpm = null;
+			try {
+				persistableSession = getPersistableSession(request);
+				wpm = getWorkflowPersistenceManager(persistableSession);
+				wpm.setWorkflowCallbackHandler(new FullReviewedWorkflowCallbackHandler());
+				Object object = wpm.getObject(documentCanonicalPath);
+				if(log.isInfoEnabled()){
+					log.info("have the object and Name of object"+object.toString());
+				}
+				if(object instanceof HippoBean){
+					HippoBean hippoBeanDocument = (HippoBean) object;
+					FormMap formMap = getFormMap(object, request);
+					for(String fieldName:formMap.getFieldNames()){
+						if(log.isInfoEnabled()){
+							log.info("Lets see field name :::"+fieldName+"::Value::"+formMap.getField(fieldName).getValue());
+						}	
+					}
+					Node documentNode = hippoBeanDocument.getNode();
+					if(!documentNode.isCheckedOut()){
+						log.info("Yes it is :::");
+						documentNode.checkout();
+					}
+					CustomContentNodeBinderImpl binderImpl = new CustomContentNodeBinderImpl(object, formMap);
+					wpm.update(object, binderImpl);
+					wpm.save();
+				}
+			} catch (RepositoryException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ObjectBeanManagerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public FormMap getFormMap(Object object, HstRequest request){
+		FormMap formMap = null;
+		if(object !=null){
+			List<String> fieldNames = new ArrayList<String>();
+			Method[] objectMehtods = object.getClass().getMethods();
+			for(Method objectMethod:objectMehtods){
+				if(objectMethod.isAnnotationPresent(AutoCreateFormField.class)){
+					AutoCreateFormField autoCreateFormField = objectMethod.getAnnotation(AutoCreateFormField.class);
+					fieldNames.add(autoCreateFormField.name());
+				}
+			}
+			formMap = new FormMap(request, fieldNames);
+		}
+		return formMap;
+	}
+}
