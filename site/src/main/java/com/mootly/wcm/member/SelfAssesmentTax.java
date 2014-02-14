9903de5f9b5689fa54e3fc2cf3d291d2739c9134
amitpatkar <amit@mootly.com>
2014-02-13 16:05:05 -0800

    Revert "Attempting to commit Pankaj's changes"
    This reverts commit c75e3e280879f2e67d41ce916be40642d3f6c47f.

--- site/src/main/java/com/mootly/wcm/member/SelfAssesmentTax.java
+++ site/src/main/java/com/mootly/wcm/member/SelfAssesmentTax.java
@@ -1,5 +1,6 @@
 package com.mootly.wcm.member;
 
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -28,34 +29,37 @@ import com.mootly.wcm.services.ITRXmlGeneratorServiceCommon;
  * Date:13/3/2013
  * Description:It will taje value from Tdsfromsalary.jsp and pass it to bean
  */
-@PrimaryBean(primaryBeanClass = SelfAssesmetTaxDocument.class)
-@ChildBean(childBeanClass = SelfAssesmentTaxDetail.class)
-@FormFields(fieldNames = { "bsr_codeself", "date_creditself",
-		"Serial_challanself", "amountself", "isImportedFromDIT" })
-@RequiredFields(fieldNames = { "bsr_codeself", "date_creditself",
-		"Serial_challanself", "amountself" })
-@DataTypeValidationFields(fieldNames = { "bsr_codeself", "Serial_challanself",
-		"amountself", "date_creditself" }, dataTypes = {
-		DataTypeValidationType.BSR, DataTypeValidationType.CHALLANNO,
-		DataTypeValidationType.DECIMAL, DataTypeValidationType.INDIANDATE })
+@PrimaryBean(primaryBeanClass=SelfAssesmetTaxDocument.class)
+@ChildBean(childBeanClass=SelfAssesmentTaxDetail.class)
+@FormFields(fieldNames={"bsr_codeself","date_creditself","Serial_challanself","amountself"})
+@RequiredFields(fieldNames={"bsr_codeself","date_creditself","Serial_challanself","amountself"})
+@DataTypeValidationFields(fieldNames={
+		"bsr_codeself",
+		"Serial_challanself",
+		"amountself",
+		"date_creditself"
+		},
+		dataTypes={
+		DataTypeValidationType.BSR,
+		DataTypeValidationType.CHALLANNO,
+		DataTypeValidationType.DECIMAL,
+		DataTypeValidationType.INDIANDATE
+})
 public class SelfAssesmentTax extends ITReturnComponent {
 
-	private static final Logger log = LoggerFactory
-			.getLogger(TdsFromSalary.class);
-
+	private static final Logger log = LoggerFactory.getLogger(TdsFromSalary.class);
 	public void doBeforeRender(HstRequest request, HstResponse response) {
 		// TODO Auto-generated method stub
 		super.doBeforeRender(request, response);
-		if (log.isInfoEnabled()) {
+		if(log.isInfoEnabled()){
 			log.info("this is do before render of self assesment tax");
 		}
 
-		DateFormat formatter;
+		DateFormat formatter ;
 		Date currentDate = null;
 		formatter = new SimpleDateFormat("yyyy-MM-dd");
 		try {
-			currentDate = formatter.parse(ITRXmlGeneratorServiceCommon
-					.getCurrentDateInIndiaAsString());
+			currentDate = formatter.parse(ITRXmlGeneratorServiceCommon.getCurrentDateInIndiaAsString());
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -63,21 +67,23 @@ public class SelfAssesmentTax extends ITReturnComponent {
 
 		formatter = new SimpleDateFormat("dd/MM/yyy");
 		String currentDateStr = formatter.format(currentDate);
-		FinancialYear financialYear = (FinancialYear) request
-				.getAttribute("financialYear");
+		FinancialYear financialYear = (FinancialYear) request.getAttribute("financialYear");
 		int endYear = financialYear.getEndYear();
 		request.setAttribute("endYear", endYear);
 		request.setAttribute("currDate", currentDateStr);
 	}
-
 	@Override
 	public void doAction(HstRequest request, HstResponse response)
 			throws HstComponentException {
 
 		// TODO Auto-generated method stub
 		super.doAction(request, response);
-		if (log.isInfoEnabled()) {
-			log.info("this is do Action of self assesment tax");
+		if(log.isInfoEnabled()){
+		log.info("this is do Action of self assesment tax");
 		}
 	}
 }
+
+
+
+
--- site/src/main/java/com/mootly/wcm/member/Tcs.java
+++ site/src/main/java/com/mootly/wcm/member/Tcs.java
@@ -26,20 +26,27 @@ import com.mootly.wcm.beans.MemberPersonalInformation;
 import com.mootly.wcm.beans.TcsDocument;
 import com.mootly.wcm.beans.compound.TcsDetail;
 import com.mootly.wcm.components.ITReturnComponent;
+@PrimaryBean(primaryBeanClass=TcsDocument.class)
+@ChildBean(childBeanClass=TcsDetail.class)
+@AdditionalBeans(additionalBeansToLoad=MemberPersonalInformation.class)
+@RequiredBeans(requiredBeans={MemberPersonalInformation.class})
+@RequiredFields(fieldNames={"tan","name","totaltax"})
+@FormFields(fieldNames={"tan","name","totaltax","taxCredited"})
+@DataTypeValidationFields(fieldNames={
+		"tan",
+		"totaltax","amtCredited"
 
-@PrimaryBean(primaryBeanClass = TcsDocument.class)
-@ChildBean(childBeanClass = TcsDetail.class)
-@AdditionalBeans(additionalBeansToLoad = MemberPersonalInformation.class)
-@RequiredBeans(requiredBeans = { MemberPersonalInformation.class })
-@RequiredFields(fieldNames = { "tan", "name", "totaltax" })
-@FormFields(fieldNames = { "tan", "name", "totaltax", "taxCredited",
-		"isImportedFromDIT" })
-@DataTypeValidationFields(fieldNames = { "tan", "totaltax", "amtCredited"
+},
+dataTypes= {
+		DataTypeValidationType.TAN, //Gross_salary
+		DataTypeValidationType.DECIMAL,
+		DataTypeValidationType.DECIMAL,
 
-}, dataTypes = { DataTypeValidationType.TAN, // Gross_salary
-		DataTypeValidationType.DECIMAL, DataTypeValidationType.DECIMAL,
+}
+		)
 
-})
+
+
 public class Tcs extends ITReturnComponent {
 
 	private static final Logger log = LoggerFactory.getLogger(Tcs.class);
@@ -49,8 +56,7 @@ public class Tcs extends ITReturnComponent {
 		// TODO Auto-generated method stub
 		super.doBeforeRender(request, response);
 
-		// String date1= ContentStructure.getMemberAssetDocPath(, filing_year,
-		// getUserName());
+		//String date1= ContentStructure.getMemberAssetDocPath(, filing_year, getUserName());
 
 	}
 
