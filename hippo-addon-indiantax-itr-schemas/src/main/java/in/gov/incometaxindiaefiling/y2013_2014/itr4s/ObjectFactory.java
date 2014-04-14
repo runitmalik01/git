390c4d59f358302843db01a95739ac1d1f0c83bc
amitpatkar <amit@mootly.com>
2014-04-13 23:32:36 -0700

    DJ This time the Income Tax Department is releasing the XSD in separate folders. I don't know their final plan. but lets see if this approach works for us. When you need to generate ITR1 please go ahead use the namespace ITR1

---
+++ hippo-addon-indiantax-itr-schemas/src/main/java/in/gov/incometaxindiaefiling/y2013_2014/itr4s/ObjectFactory.java
@@ -0,0 +1,397 @@
+
+package in.gov.incometaxindiaefiling.y2013_2014.itr4s;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the in.gov.incometaxindiaefiling.y2013_2014.itr4s package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _CountryCode_QNAME = new QName("http://incometaxindiaefiling.gov.in/master", "CountryCode");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: in.gov.incometaxindiaefiling.y2013_2014.itr4s
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Address }
+     * 
+     */
+    public Address createAddress() {
+        return new Address();
+    }
+
+    /**
+     * Create an instance of {@link Refund }
+     * 
+     */
+    public Refund createRefund() {
+        return new Refund();
+    }
+
+    /**
+     * Create an instance of {@link Schedule80G }
+     * 
+     */
+    public Schedule80G createSchedule80G() {
+        return new Schedule80G();
+    }
+
+    /**
+     * Create an instance of {@link Verification }
+     * 
+     */
+    public Verification createVerification() {
+        return new Verification();
+    }
+
+    /**
+     * Create an instance of {@link NatOfBus }
+     * 
+     */
+    public NatOfBus createNatOfBus() {
+        return new NatOfBus();
+    }
+
+    /**
+     * Create an instance of {@link ScheduleTCS }
+     * 
+     */
+    public ScheduleTCS createScheduleTCS() {
+        return new ScheduleTCS();
+    }
+
+    /**
+     * Create an instance of {@link NatOfBus.Business }
+     * 
+     */
+    public NatOfBus.Business createNatOfBusBusiness() {
+        return new NatOfBus.Business();
+    }
+
+    /**
+     * Create an instance of {@link ITR4S }
+     * 
+     */
+    public ITR4S createITR4S() {
+        return new ITR4S();
+    }
+
+    /**
+     * Create an instance of {@link CreationInfo }
+     * 
+     */
+    public CreationInfo createCreationInfo() {
+        return new CreationInfo();
+    }
+
+    /**
+     * Create an instance of {@link FormITR4S }
+     * 
+     */
+    public FormITR4S createFormITR4S() {
+        return new FormITR4S();
+    }
+
+    /**
+     * Create an instance of {@link PersonalInfo }
+     * 
+     */
+    public PersonalInfo createPersonalInfo() {
+        return new PersonalInfo();
+    }
+
+    /**
+     * Create an instance of {@link AssesseeName }
+     * 
+     */
+    public AssesseeName createAssesseeName() {
+        return new AssesseeName();
+    }
+
+    /**
+     * Create an instance of {@link Address.Phone }
+     * 
+     */
+    public Address.Phone createAddressPhone() {
+        return new Address.Phone();
+    }
+
+    /**
+     * Create an instance of {@link FilingStatus }
+     * 
+     */
+    public FilingStatus createFilingStatus() {
+        return new FilingStatus();
+    }
+
+    /**
+     * Create an instance of {@link AssesseeRep }
+     * 
+     */
+    public AssesseeRep createAssesseeRep() {
+        return new AssesseeRep();
+    }
+
+    /**
+     * Create an instance of {@link ITR4SIncomeDeductions }
+     * 
+     */
+    public ITR4SIncomeDeductions createITR4SIncomeDeductions() {
+        return new ITR4SIncomeDeductions();
+    }
+
+    /**
+     * Create an instance of {@link UsrDeductUndChapVIA }
+     * 
+     */
+    public UsrDeductUndChapVIA createUsrDeductUndChapVIA() {
+        return new UsrDeductUndChapVIA();
+    }
+
+    /**
+     * Create an instance of {@link DeductUndChapVIA }
+     * 
+     */
+    public DeductUndChapVIA createDeductUndChapVIA() {
+        return new DeductUndChapVIA();
+    }
+
+    /**
+     * Create an instance of {@link ITR4STaxComputation }
+     * 
+     */
+    public ITR4STaxComputation createITR4STaxComputation() {
+        return new ITR4STaxComputation();
+    }
+
+    /**
+     * Create an instance of {@link IntrstPay }
+     * 
+     */
+    public IntrstPay createIntrstPay() {
+        return new IntrstPay();
+    }
+
+    /**
+     * Create an instance of {@link TaxPaid }
+     * 
+     */
+    public TaxPaid createTaxPaid() {
+        return new TaxPaid();
+    }
+
+    /**
+     * Create an instance of {@link TaxesPaid }
+     * 
+     */
+    public TaxesPaid createTaxesPaid() {
+        return new TaxesPaid();
+    }
+
+    /**
+     * Create an instance of {@link Refund.DepositToBankAccount }
+     * 
+     */
+    public Refund.DepositToBankAccount createRefundDepositToBankAccount() {
+        return new Refund.DepositToBankAccount();
+    }
+
+    /**
+     * Create an instance of {@link Schedule80G.Don100Percent }
+     * 
+     */
+    public Schedule80G.Don100Percent createSchedule80GDon100Percent() {
+        return new Schedule80G.Don100Percent();
+    }
+
+    /**
+     * Create an instance of {@link Schedule80G.Don50PercentNoApprReqd }
+     * 
+     */
+    public Schedule80G.Don50PercentNoApprReqd createSchedule80GDon50PercentNoApprReqd() {
+        return new Schedule80G.Don50PercentNoApprReqd();
+    }
+
+    /**
+     * Create an instance of {@link Schedule80G.Don100PercentApprReqd }
+     * 
+     */
+    public Schedule80G.Don100PercentApprReqd createSchedule80GDon100PercentApprReqd() {
+        return new Schedule80G.Don100PercentApprReqd();
+    }
+
+    /**
+     * Create an instance of {@link Schedule80G.Don50PercentApprReqd }
+     * 
+     */
+    public Schedule80G.Don50PercentApprReqd createSchedule80GDon50PercentApprReqd() {
+        return new Schedule80G.Don50PercentApprReqd();
+    }
+
+    /**
+     * Create an instance of {@link Verification.Declaration }
+     * 
+     */
+    public Verification.Declaration createVerificationDeclaration() {
+        return new Verification.Declaration();
+    }
+
+    /**
+     * Create an instance of {@link TaxReturnPreparer }
+     * 
+     */
+    public TaxReturnPreparer createTaxReturnPreparer() {
+        return new TaxReturnPreparer();
+    }
+
+    /**
+     * Create an instance of {@link ScheduleBPForITR4S }
+     * 
+     */
+    public ScheduleBPForITR4S createScheduleBPForITR4S() {
+        return new ScheduleBPForITR4S();
+    }
+
+    /**
+     * Create an instance of {@link PersumptiveInc44AD }
+     * 
+     */
+    public PersumptiveInc44AD createPersumptiveInc44AD() {
+        return new PersumptiveInc44AD();
+    }
+
+    /**
+     * Create an instance of {@link PersumptiveInc44AE }
+     * 
+     */
+    public PersumptiveInc44AE createPersumptiveInc44AE() {
+        return new PersumptiveInc44AE();
+    }
+
+    /**
+     * Create an instance of {@link NoBooksOfAccBS }
+     * 
+     */
+    public NoBooksOfAccBS createNoBooksOfAccBS() {
+        return new NoBooksOfAccBS();
+    }
+
+    /**
+     * Create an instance of {@link ScheduleIT }
+     * 
+     */
+    public ScheduleIT createScheduleIT() {
+        return new ScheduleIT();
+    }
+
+    /**
+     * Create an instance of {@link TaxPayment }
+     * 
+     */
+    public TaxPayment createTaxPayment() {
+        return new TaxPayment();
+    }
+
+    /**
+     * Create an instance of {@link ScheduleTCS.TCS }
+     * 
+     */
+    public ScheduleTCS.TCS createScheduleTCSTCS() {
+        return new ScheduleTCS.TCS();
+    }
+
+    /**
+     * Create an instance of {@link TDSonSalaries }
+     * 
+     */
+    public TDSonSalaries createTDSonSalaries() {
+        return new TDSonSalaries();
+    }
+
+    /**
+     * Create an instance of {@link TDSonSalary }
+     * 
+     */
+    public TDSonSalary createTDSonSalary() {
+        return new TDSonSalary();
+    }
+
+    /**
+     * Create an instance of {@link EmployerOrDeductorOrCollectDetl }
+     * 
+     */
+    public EmployerOrDeductorOrCollectDetl createEmployerOrDeductorOrCollectDetl() {
+        return new EmployerOrDeductorOrCollectDetl();
+    }
+
+    /**
+     * Create an instance of {@link TDSonOthThanSals }
+     * 
+     */
+    public TDSonOthThanSals createTDSonOthThanSals() {
+        return new TDSonOthThanSals();
+    }
+
+    /**
+     * Create an instance of {@link TDSonOthThanSal }
+     * 
+     */
+    public TDSonOthThanSal createTDSonOthThanSal() {
+        return new TDSonOthThanSal();
+    }
+
+    /**
+     * Create an instance of {@link DoneeWithPan }
+     * 
+     */
+    public DoneeWithPan createDoneeWithPan() {
+        return new DoneeWithPan();
+    }
+
+    /**
+     * Create an instance of {@link AddressDetail }
+     * 
+     */
+    public AddressDetail createAddressDetail() {
+        return new AddressDetail();
+    }
+
+    /**
+     * Create an instance of {@link NatOfBus.Business.Trade }
+     * 
+     */
+    public NatOfBus.Business.Trade createNatOfBusBusinessTrade() {
+        return new NatOfBus.Business.Trade();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://incometaxindiaefiling.gov.in/master", name = "CountryCode")
+    public JAXBElement<String> createCountryCode(String value) {
+        return new JAXBElement<String>(_CountryCode_QNAME, String.class, null, value);
+    }
+
+}
