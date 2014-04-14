3d09881c902f1938398b54f4f777f2be2f5afba0
amitpatkar <amit@mootly.com>
2014-04-14 13:11:11 -0700

    Node JS Project It can act as a wrapper to various other functions and can be invoked by the Java Code and web properties.

---
+++ nodejsprojects/reportui/app.js
@@ -0,0 +1,52 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+mcapi = require('./node_modules/mailchimp-api/mailchimp')
+
+// New Code
+var mongo = require('mongodb');
+var monk = require('monk');
+var mongoskin = require('mongoskin');
+
+var app = express();
+
+db = mongoskin.db('mongodb://w4indiaprod:mootly007@wealth4india.com:27017/w4india');
+// set MailChimp API key here
+mc = new mcapi.Mailchimp('7da5576ec81d13572c3008ac81fd50af-us8');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+//app.get('/users', user.list);
+app.get('/inactivemembers', routes.memberlist(db,false));
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
